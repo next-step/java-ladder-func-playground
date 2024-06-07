35258f8eb938b3ddd1e0f34e56ff02a641ada803
승엽 <tmdduqflfl@naver.com>
2024-06-07 21:57:37 +0900

    Fix: - Application 에서 실행할 수 있도록 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import controller.Controller;
-
 public class Application {
 
-    static Controller controller = new Controller();
-
-    public static void main(String[] args) {
-        controller.run();
-    }
-=======
-public class Application {
-
->>>>>>> 0c46fef (Feat:)
 }

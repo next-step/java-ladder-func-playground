12255e6608c53201ab134ba3f14fe0b2244cb9b7
승엽 <tmdduqflfl@naver.com>
2024-06-07 22:04:10 +0900

    Fix: - 불필요한 테스트 삭제

--- src/test/java/model/RowLinesTest.java
+++ src/test/java/model/RowLinesTest.java
@@ -1,28 +1,5 @@
 package model;
 
-<<<<<<< HEAD
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class RowLinesTest {
-
-    @Test
-    void 열사다리_생성_테스트() {
-
-        // given
-        int height = 3;
-        int width = 5;
-
-        // when
-        RowLines rowLines = RowLines.of(height, width);
-
-        // then
-        assertEquals(height, rowLines.getRowLines().size());
-        for (Lines lines : rowLines.getRowLines()) {
-            assertEquals(width, lines.getLines().size());
-        }
-=======
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +15,5 @@ class RowLinesTest {
 
         // when&then
         Assertions.assertEquals(rowLines1, rowLines);
->>>>>>> 6676a85 (Feat:)
     }
 }

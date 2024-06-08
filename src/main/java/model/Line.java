e25174e6ee4bbfc7b34d7ad5ca17570acacf7bbb
승엽 <tmdduqflfl@naver.com>
2024-06-08 19:39:41 +0900

    Fix: - private public 정렬

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -4,13 +4,10 @@ public class Line {
 
     private final boolean hasPedal;
 
-<<<<<<< HEAD
-=======
     private Line(boolean hasPedal) {
         this.hasPedal = hasPedal;
     }
 
->>>>>>> 0c46fef (Feat:)
     public static Line of(boolean hasPoint) {
         return new Line(hasPoint);
     }
@@ -18,11 +15,4 @@ public class Line {
     public boolean hasPedal() {
         return hasPedal;
     }
-<<<<<<< HEAD
-
-    private Line(boolean hasPedal) {
-        this.hasPedal = hasPedal;
-    }
-=======
->>>>>>> 0c46fef (Feat:)
 }

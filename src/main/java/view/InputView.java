c1951b193d2871e40cf74d4b7f4c7a7a25b9ca3e
승엽 <tmdduqflfl@naver.com>
2024-06-16 17:38:19 +0900

    Feat: - 사람이름_출력테스트& 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,18 +1,9 @@
 package view;
 
-<<<<<<< HEAD
-import java.util.List;
-=======
->>>>>>> 0c46fef (Feat:)
 import java.util.Scanner;
 
 public class InputView {
 
-<<<<<<< HEAD
-    private static final String SPLIT_COMMA = ",";
-
-=======
->>>>>>> 0c46fef (Feat:)
     private final Scanner scanner;
 
     public InputView(final Scanner scanner) {
@@ -28,22 +19,4 @@ public class InputView {
         System.out.println("사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
     }
-<<<<<<< HEAD
-
-    public List<String> personName() {
-        System.out.println("참여할 사람이름을 입력하세요(쉼표로 구분):");
-        return List.of(scanner.next().split(SPLIT_COMMA));
-    }
-
-    public List<String> winningResult() {
-        System.out.println("실행 결과를 입력하세요 (쉼표로구분)");
-        return List.of(scanner.next().split(SPLIT_COMMA));
-    }
-
-    public String askResultPerson() {
-        System.out.println("결과를 보고 싶은 사람은?");
-        return scanner.next();
-    }
-=======
->>>>>>> 0c46fef (Feat:)
 }

34c590b267d158dc3501c4db86eff0aff243a8c2
승엽 <tmdduqflfl@naver.com>
2024-06-16 17:38:19 +0900

    Feat: - 사람이름_출력테스트& 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -18,5 +19,15 @@ public class InputView {
     public int heightLadder() {
         System.out.println("사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
+    }
+
+    public List<String> personName() {
+        System.out.println("참여할 사람이름을 입력하세요(쉼표로 구분):");
+        return List.of(scanner.next().split(","));
+    }
+
+    public List<String> winningResult() {
+        System.out.println("실행 결과를 입력하세요 (쉼표로구분)");
+        return List.of(scanner.next().split(","));
     }
 }

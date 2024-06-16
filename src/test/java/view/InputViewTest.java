aad9b26c003a867a3a747b0372a5e87fa2a6401e
승엽 <tmdduqflfl@naver.com>
2024-06-16 19:32:20 +0900

    Feat: - 메서드 구현 테스트 작성

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -2,6 +2,7 @@ package view;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,5 +21,19 @@ class InputViewTest {
         // then
         assertEquals(3, inputView.widthLadder());
         assertEquals(5, inputView.heightLadder());
+    }
+
+    @Test
+    void 사람이름_출력테스트() {
+
+        // given
+        Scanner scanner = new Scanner("done,skr,add,e");
+
+        // when
+        InputView inputView = new InputView(scanner);
+        List<String> expectName= List.of(new String[]{"done", "skr", "add", "e"});
+
+        // then
+        assertEquals(inputView.personName(),expectName);
     }
 }

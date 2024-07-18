00bbda92d1062b93535850f9ce9eeeec937cb361
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-07-18 20:30:32 +0900

    3단계 - 사다리 타기

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,20 +1,59 @@
 package domain;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import view.InputView;
 import view.OutputView;
 
 public class LadderGame {
 
     private final LadderMaker ladderMaker = new LadderMaker();
+    private Ladder ladder;
 
     public void runGame() {
         int width = InputView.printWidthCount();
         int height = InputView.printHeightCount();
-        Ladder ladder = createLadder(width, height);
+        ladder = createLadder(width, height);
+        List<Integer> users = makeUser(width);
+        List<Integer> result = calculateResult(users);
         OutputView.printLadder(ladder);
+        OutputView.printResult(users, result);
     }
 
-    public Ladder createLadder(int width, int height) {
+    private Ladder createLadder(int width, int height) {
         return ladderMaker.createLadder(width, height);
     }
+
+    private List<Integer> makeUser(int width) {
+        return IntStream.range(0, width)
+            .boxed()
+            .toList();
+    }
+
+    private List<Integer> calculateResult(List<Integer> users) {
+        return users.stream()
+            .map(this::climbLadder)
+            .toList();
+    }
+
+    private int climbLadder(int user) {
+        List<Line> lines = ladder.getLines();
+        for (Line line : lines) {
+            user = move(user, line);
+        }
+        return user;
+    }
+
+    public int move(int user, Line line) {
+        List<Boolean> points = line.getPoints();
+        if (points.get(user)) {
+            return ++user;
+        }
+        if (user > 0 && points.get(user - 1)) {
+            return --user;
+        }
+        return user;
+    }
+
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,7 +9,7 @@ import domain.Line;
 public class OutputView {
 
     public static void printLadder(Ladder ladder) {
-        System.out.println("실행 결과\n");
+        System.out.println("\n실행 결과\n");
         List<Line> lines = ladder.getLines();
         lines.forEach(line ->
             System.out.println(drawLine(line.getPoints()))
@@ -25,5 +25,12 @@ public class OutputView {
                 return "|    ";
             })
             .collect(Collectors.joining());
+    }
+
+    public static void printResult(List<Integer> users, List<Integer> result) {
+        System.out.println();
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(String.format("%d -> %d", users.get(i), result.get(i)));
+        }
     }
 }

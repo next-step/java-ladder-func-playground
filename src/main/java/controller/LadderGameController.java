31f8439cc22614ca8d93ec4869883945ad354c7b
JoungMinJu <alswn9938@gmail.com>
2024-10-07 22:24:16 +0900

    step2 : 너비, 높이 받는 로직 추가

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -5,24 +5,39 @@ import domain.Height;
 import domain.Ladders;
 import domain.RungsBuilder;
 import service.LaddersService;
+import view.InputView;
 import view.OutputView;
 
 public class LadderGameController {
 
     private final LaddersService laddersService;
     private final OutputView outputView;
+    private final InputView inputView;
 
     public LadderGameController(RungsBuilder rungsBuilder) {
         this.laddersService = new LaddersService(rungsBuilder);
         this.outputView = new OutputView();
+        this.inputView = new InputView();
     }
 
     public void start() {
-        CountOfLadders countOfLadders = new CountOfLadders(3);
-        Height height = new Height(6);
+        CountOfLadders countOfLadders = getCountOfLadders();
+        Height height = getHeight();
 
         Ladders ladders = laddersService.createLadders(countOfLadders, height);
         outputView.printStatusOfLadders(ladders.getRungsStatusAtLadder(), height.value());
+    }
+
+    private CountOfLadders getCountOfLadders() {
+        outputView.printInputCountOfLaddersGuide();
+        final int valueOfCountOfLadders = inputView.getUserIntegerInput();
+        return new CountOfLadders(valueOfCountOfLadders);
+    }
+
+    private Height getHeight() {
+        outputView.printInputHeightGuide();
+        final int valueOfHeight = inputView.getUserIntegerInput();
+        return new Height(valueOfHeight);
     }
 
 }

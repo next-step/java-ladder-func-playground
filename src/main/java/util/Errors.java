6f1e2939a6248c857d5c464b987155c32d0fb1f1
JoungMinJu <alswn9938@gmail.com>
2024-10-07 22:24:06 +0900

    step2 : InputView 생성

--- src/main/java/util/Errors.java
+++ src/main/java/util/Errors.java
@@ -2,6 +2,7 @@ package util;
 
 public class Errors {
 
+
     private Errors() {
     }
 
@@ -9,5 +10,7 @@ public class Errors {
     public static final String LADDER_HEIGHT_MUST_BE_POSITIVE = "사다리의 높이는 1 이상이어야 합니다.";
     public static final String ALL_LADDERS_MUST_HAVE_SAME_HEIGHT = "각 사다리의 높이는 모두 동일해야합니다.";
     public static final String ADJACENT_LADDERS_CANNOT_HAVE_RUNG_AT_SAME_POSITION = "인접한 두 사다리는 같은 위치에 가로줄을 가질 수 없습니다.";
+    public static final String INPUT_IS_NOT_INTEGER = "정수가 입력되어야 합니다.";
+
 
 }

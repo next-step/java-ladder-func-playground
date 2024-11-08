80427aebc123ba32ee386595fbde1b211f3193a1
goldm0ng <jujs45177@gmail.com>
2024-11-08 13:53:33 +0900

    <FIX> 자바 컨벤션에 따른 메서드 이름 수정

--- src/main/java/model/player/Players.java
+++ src/main/java/model/player/Players.java
@@ -53,7 +53,7 @@ public class Players {
                 .orElseThrow(() -> new IllegalArgumentException("찾고자 하는 플레이어가 존재하지 않습니다."));
     }
 
-    public int Count(){
+    public int count(){
         return players.size();
     }
 

a64f240dd022148ba03c7391b629942982598631
dldb-chamchi <dladkfl49@gmail.com>
2025-02-18 20:03:43 +0900

    feat: model -> Player

---
+++ src/main/java/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    private final String name;
+
+    public Player(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지 가능합니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

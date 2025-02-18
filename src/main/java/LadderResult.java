6ffe44dbaa1959b9eefaa274ef02caba4ec489d4
dldb-chamchi <dladkfl49@gmail.com>
2025-02-18 20:06:31 +0900

    feat: model -> LadderResult

---
+++ src/main/java/LadderResult.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class LadderResult {
+    private final Map<String, String> results;
+
+    public LadderResult(Map<String, String> results) {
+        this.results = results;
+    }
+
+    public String getResult(String name) {
+        return results.get(name);
+    }
+
+    public Map<String, String> getAllResults() {
+        return results;
+    }
+}

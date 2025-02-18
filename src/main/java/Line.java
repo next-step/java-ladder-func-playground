06c76df36fd2b50d80ade3deb262bb2d515ce05b
dldb-chamchi <dladkfl49@gmail.com>
2025-02-18 20:09:21 +0900

    feat: model -> Line

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,2 +1,34 @@
+import java.util.*;
+
 public class Line {
+    private final List<Boolean> points;
+
+    public Line(int count) {
+        points = new ArrayList<>(Collections.nCopies(count - 1, false));
+        generateRandomLadder();
+    }
+
+    private void generateRandomLadder() {
+        Random random = new Random();
+        for (int i = 0; i < points.size() - 1; i++) {
+            if (!points.get(i) && (i == 0 || !points.get(i - 1))) {
+                points.set(i, random.nextBoolean());
+            }
+        }
+    }
+
+    public int move(int position) {
+        if (position > 0 && points.get(position - 1)) {
+            return position - 1;
+        }
+        if (position < points.size() && points.get(position)) {
+            return position + 1;
+        }
+        return position;
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
 }
+

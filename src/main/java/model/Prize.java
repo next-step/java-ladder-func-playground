7a08d40355edbd96c8b2866c99fe6d785548a81a
jihoo2002 <yolohoo@naver.com>
2025-03-19 10:26:13 +0900

    refactor Prize 클래스 마지막줄 개행

--- src/main/java/model/Prize.java
+++ src/main/java/model/Prize.java
@@ -14,9 +14,8 @@ public class Prize {
     }
 
     private void validateValue(String value) {
-        if(value == null || value.isEmpty()) {
+        if (value == null || value.isEmpty()) {
             throw new IllegalArgumentException("실행결과는 null이거나 공백일 수는 없습니다.");
         }
     }
 }
-

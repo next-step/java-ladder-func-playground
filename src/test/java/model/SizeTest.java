3a1f20833c51a050b265fc1b06f678101a0cbc7e
jihoo2002 <yolohoo@naver.com>
2025-03-19 16:15:46 +0900

    refactor SizeTest displayname 리펙토링

--- src/test/java/model/SizeTest.java
+++ src/test/java/model/SizeTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 public class SizeTest {
 
     @Test
-    @DisplayName("사이즈가 유효한 값이면 Size 객체를 생성하는지 검증한다.")
+    @DisplayName("사다리 크기가 유효한 값이면 정상적으로 객체를 생성한다.")
     public void shouldCreateSizeWithValidValue() {
         int validSize = 5;
         Size size = new Size(validSize);
@@ -15,7 +15,7 @@ public class SizeTest {
     }
 
     @Test
-    @DisplayName("사이즈가 유효하지 않는 값이면 예외를 발생시키는지 검증한다.")
+    @DisplayName("사다리 크기가 유효하지 않는 값이면 예외가 발생한다.")
     public void shouldThrowExceptionWhenSizeIsNegative() {
         int invalidSize = -1;
 

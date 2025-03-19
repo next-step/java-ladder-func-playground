a2a5707029e8c4cb24035b15809aad83bcb3e709
jihoo2002 <yolohoo@naver.com>
2025-03-19 15:23:21 +0900

    refactor HeightTest displayname 리펙토링

--- src/test/java/model/HeightTest.java
+++ src/test/java/model/HeightTest.java
@@ -7,21 +7,21 @@ import static org.assertj.core.api.Assertions.*;
 class HeightTest {
 
     @Test
-    @DisplayName("Height 객체의 높이가 유효한 값일 때 정상적으로 생성되는지 검증한다.")
+    @DisplayName("높이가 유효한 값일 때 정상적으로 생성된다.")
     void shouldCreateHeightWithValidValue() {
         Height height = new Height(5);
         assertThat(height.getValue()).isEqualTo(5);
     }
 
     @Test
-    @DisplayName("최소값일 때 Height 객체가 정상적으로 생성되는지 검증한다.")
+    @DisplayName("최소값일 때 높이가 정상적으로 생성된다")
     void shouldCreateHeightWithMinimumValue() {
         Height height = new Height(2);
         assertThat(height.getValue()).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("Height 객체의 높이가 2 미만일 때 예외가 발생하는지 검증한다.")
+    @DisplayName("높이가 2 미만일 때 예외가 발생한다.")
     void shouldThrowExceptionForHeightBelowMin() {
         assertThatThrownBy(() -> new Height(1))
                 .isInstanceOf(IllegalArgumentException.class)

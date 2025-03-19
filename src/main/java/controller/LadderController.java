26d4ae4996d70829f5f0ba23b53a6fbd5158b992
jihoo2002 <yolohoo@naver.com>
2025-03-19 09:41:05 +0900

    refactor 플레이어 수에 따라 리스트를 나눠 라인 생성

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -3,8 +3,10 @@ package controller;
 import model.*;
 import view.InputView;
 import view.ResultView;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static model.Point.HAS_POINT;
 
 public class LadderController {
@@ -15,13 +17,13 @@ public class LadderController {
 
     public void startLadder() {
         Players player = new Players(inputView.inputNames());
-        Prizes prizes = Prizes.form(inputView.inputResult(), player);
+        Prizes prizes = Prizes.from(inputView.inputResult(), player);
         Height height = new Height(inputView.getMaxLadderHeight());
         PointGenerator pointGenerator = new PointGenerator(new RandomValueGenerator());
         LadderGame ladderGame = LadderGame.createGame(player, height, pointGenerator, prizes);
 
         List<Boolean> points = formatLadderPoints(ladderGame.getLadderPoints());
-        List<List<Boolean>> ladderLines = processLadderLines(points);
+        List<List<Boolean>> ladderLines = processLadderLines(points, player.size());
         resultView.printLadder(ladderLines, player.getPlayers(), prizes.getPrize());
         printResult(ladderGame);
     }
@@ -48,15 +50,16 @@ public class LadderController {
         return formattedLadder;
     }
 
-    private List<List<Boolean>> processLadderLines(List<Boolean> ladderPoints) {
+    private List<List<Boolean>> processLadderLines(List<Boolean> ladderPoints, int numPlayers) {
+        int lineCount = numPlayers - 1;
         List<List<Boolean>> chunks = new ArrayList<>();
-        for (int i = 0; i < ladderPoints.size(); i += CHUNK_SIZE) {
-            chunks.add(getLadderSegment(ladderPoints, i));
+        for (int i = 0; i < ladderPoints.size(); i += lineCount) {
+            chunks.add(getLadderSegment(ladderPoints, i, lineCount));
         }
         return chunks;
     }
 
-    private List<Boolean> getLadderSegment(List<Boolean> ladderPoints, int startIndex) {
-        return ladderPoints.subList(startIndex, Math.min(startIndex + CHUNK_SIZE, ladderPoints.size()));
+    private List<Boolean> getLadderSegment(List<Boolean> ladderPoints, int startIndex, int lineCount) {
+        return ladderPoints.subList(startIndex, Math.min(startIndex + lineCount, ladderPoints.size()));
     }
 }

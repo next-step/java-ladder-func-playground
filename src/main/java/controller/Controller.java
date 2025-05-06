94bdd562774a06b420626bb6b56a891b6f10c123
gjtjrl303 <gjtjrl303@naver.com>
2025-05-06 15:26:44 +0900

    chore: 불필요한 import 제거

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -5,7 +5,6 @@ import view.InputView;
 import view.OutputView;
 
 import java.util.List;
-import java.util.Map;
 
 public class Controller {
 
--- src/test/java/domain/FixedBooleanGenerator.java
+++ src/test/java/domain/FixedBooleanGenerator.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 public class FixedBooleanGenerator implements BooleanGenerator {
 

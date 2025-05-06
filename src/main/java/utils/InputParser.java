b246039cbc9eb39b55adb88144aa5444e2e65bf2
gjtjrl303 <gjtjrl303@naver.com>
2025-05-06 15:36:19 +0900

    feat: 입력값 검증하는 로직 InputParser에 추가

--- src/main/java/utils/InputParser.java
+++ src/main/java/utils/InputParser.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class InputParser {
     public static List<String> parseByComma(String input) {
-        return List.of(input.split(","));
+        List<String> split = List.of(input.split(","));
+        for (String name : split) {
+            validate(name);
+        }
+        return split;
+    }
+
+    private static void validate(String name) {
+        if (name == null || name.trim().isBlank()) {
+            throw new IllegalArgumentException("입력이 null이거나 공백으로만 구성될 수 없습니다.");
+        }
     }
 }

c33eba62cfe239e0773597a7f4e93ad668c01951
SanghyunLee <lgw9736@naver.com>
2025-05-28 16:03:16 +0900

    refactor: 사다리 상태 조회

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,16 +5,35 @@ import domain.Ladder;
 import domain.Line;
 
 public class OutputView {
+    private static final String CONNECTION = "-----";
+    private static final String NOT_CONNECTION = "     ";
+    private static final String LADDER = "|";
+
     public void printLadderState(Ladder ladder) {
         System.out.println("실행결과");
         for (Line line : ladder.getLines()) {
-            StringBuilder sb = new StringBuilder();
-            for (Connection conn : line.getConnections()) {
-                sb.append("|");
-                sb.append(conn.isConnectedRight() ? "-----" : "     ");
-            }
-            sb.append("|");
-            System.out.println(sb.toString());
+            System.out.println(printLine(line));
         }
+    }
+
+    private String printLine(Line line) {
+        StringBuilder sb = new StringBuilder();
+        for (Connection conn : line.getConnections()) {
+            sb.append(LADDER);
+            sb.append(printConnection(conn));
+        }
+        sb.append(LADDER);
+        return sb.toString();
+    }
+
+    private String printConnection(Connection conn) {
+        if (conn.isConnectedRight()) {
+            return CONNECTION;
+        }
+        return NOT_CONNECTION;
+    }
+
+    public void printLadderResult(Ladder ladder) {
+
     }
 }

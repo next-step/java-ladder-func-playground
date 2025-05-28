1af8c468685a444f6073cf6da6a94c66f4cfb985
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-28 16:13:22 +0900

    test: LineTest 구현

---
+++ src/test/java/ladder/LineTest.java
@@ -0,0 +1,34 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+    @Test
+    @DisplayName("랜덤 링크 생성 시, 연속된 Link는 연결되지 않아야 한다")
+    void ShouldNotHaveConsecutiveLinksAfterRandomlyLink() {
+        // given
+        int columnCount = 6;
+        Line line = new Line(columnCount);
+
+        // when
+        line.randomlyLink(new Random());
+
+        // then
+        List<Link> links = line.getLinks();
+
+        for (int i = 1; i < links.size(); i++) {
+            boolean prevLinked = links.get(i - 1).isLinked();
+            boolean currLinked = links.get(i).isLinked();
+
+            // 연속해서 true면 실패
+            assertThat(!(prevLinked && currLinked))
+                    .isTrue();
+        }
+    }
+}

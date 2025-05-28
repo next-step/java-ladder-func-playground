b568e33efbdab7771e764a2f347fdd03f947916b
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-28 16:13:29 +0900

    test: LinkTest 구현

---
+++ src/test/java/ladder/LinkTest.java
@@ -0,0 +1,31 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LinkTest {
+    @Test
+    @DisplayName("Link 객체는 초기 생성 시 연결 상태가 false여야 한다")
+    void ShouldBeFalseWhenLinkIsCreated() {
+        // given
+        Link link = new Link();
+
+        // then
+        assertThat(link.isLinked()).isFalse();
+    }
+
+    @Test
+    @DisplayName("link() 호출 시 연결 상태가 true로 변경되어야 한다")
+    void ShouldBeTrueAfterCallingLink() {
+        // given
+        Link link = new Link();
+
+        // when
+        link.link();
+
+        // then
+        assertThat(link.isLinked()).isTrue();
+    }
+}

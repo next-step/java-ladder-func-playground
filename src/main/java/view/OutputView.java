309fa5acc5bda7c038e4b85ca1edb1b6877fba8e
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-28 15:55:15 +0900

    feat: OutputView 구현

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,35 @@
+package view;
+
+import ladder.Ladder;
+import ladder.Line;
+import ladder.Link;
+
+public class OutputView {
+    private static final String LEFT_MARGIN = "    |";
+    private static final String CONNECTED = "-----|";
+    private static final String DISCONNECTED = "     |";
+
+    private OutputView() {
+    }
+
+    public static void printLadder(Ladder ladder) {
+        System.out.println("실행결과");
+        System.out.println();
+        ladder.getLines().forEach(OutputView::printLine);
+    }
+
+    private static void printLine(Line line) {
+        System.out.print(LEFT_MARGIN);
+        line.getLinks().forEach(OutputView::printLink);
+        System.out.println();
+    }
+
+    private static void printLink(Link link) {
+        String symbol = link.isLinked() ? CONNECTED : DISCONNECTED;
+        System.out.print(symbol);
+    }
+}
+
+
+
+

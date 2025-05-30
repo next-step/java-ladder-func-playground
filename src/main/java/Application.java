cf8792bf55599d15182667a1d809cd894af09f03
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-31 00:25:14 +0900

    style: 필요없는 주석 삭제

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,9 +6,8 @@ public class Application {
     static private final int LADDER_COLUMN = 4;
 
     public static void main(String[] args) {
-        LadderGame game = new LadderGame(LADDER_ROW, LADDER_COLUMN, new RandomLinkStrategy()); //그냥 맨 사다리 만듬
+        LadderGame game = new LadderGame(LADDER_ROW, LADDER_COLUMN, new RandomLinkStrategy());
 
-        // 결과 출력
         OutputView.printLadder(game.getLadder());
     }
 }

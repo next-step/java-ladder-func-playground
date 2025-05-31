5c2d02922b2194ee1dec724ced52effe920924be
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-31 23:21:18 +0900

    test: int row, column 각각 Height, Width로 변경으로 인한테스트 리팩토링

--- src/test/java/LadderGameTest.java
+++ src/test/java/LadderGameTest.java
@@ -1,4 +1,6 @@
+import ladder.Height;
 import ladder.Ladder;
+import ladder.Width;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import strategy.RandomLinkStrategy;
@@ -10,9 +12,10 @@ class LadderGameTest {
     @DisplayName("LadderGame 생성 시 내부 Ladder에 최소 하나 이상 연결되어야 한다")
     void shouldGenerateLadderWithAtLeastOneLinked() {
         // given
-        int row = 4;
-        int column = 4;
-        LadderGame game = new LadderGame(row, column, new RandomLinkStrategy());
+        Height height = Height.from(4);
+        Width width = Width.from(4);
+
+        LadderGame game = new LadderGame(height, width, new RandomLinkStrategy());
 
         // when
         Ladder ladder = game.getLadder();
@@ -29,7 +32,10 @@ class LadderGameTest {
     @DisplayName("LadderGame에서 getLadder 호출 시 Ladder 객체를 반환해야 한다")
     void shouldReturnLadderWhenGetLadderIsCalled() {
         // given
-        LadderGame game = new LadderGame(3, 4, new RandomLinkStrategy());
+        Height height = Height.from(3);
+        Width width = Width.from(4);
+
+        LadderGame game = new LadderGame(height, width, new RandomLinkStrategy());
 
         // when
         Ladder ladder = game.getLadder();

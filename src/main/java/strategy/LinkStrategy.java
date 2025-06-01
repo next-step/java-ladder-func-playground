90193feef26534fb5f3b91da79693749a2ee97d5
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-01 19:53:41 +0900

    feat: LinkStrategy에 랜덤 선택 기능 선택적으로 지원하도록 개선

--- src/main/java/strategy/LinkStrategy.java
+++ src/main/java/strategy/LinkStrategy.java
@@ -2,4 +2,8 @@ package strategy;
 
 public interface LinkStrategy {
     boolean canLink();
+
+    default int pickRandomRow(int bound) {
+        throw new UnsupportedOperationException("랜덤 선택은 지원되지 않는 전략입니다.");
+    }
 }

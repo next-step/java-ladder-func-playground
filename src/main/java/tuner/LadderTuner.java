17da28eb29fec62673c0fa6307bbd2cbf3771dba
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-04 23:17:34 +0900

    fix: 매개변수 추가

--- src/main/java/tuner/LadderTuner.java
+++ src/main/java/tuner/LadderTuner.java
@@ -1,10 +1,12 @@
 package tuner;
 
+import ladder.Height;
+import ladder.Ladder;
 import ladder.Line;
 import ladder.Width;
 
 import java.util.List;
 
 public interface LadderTuner {
-    void tune(List<Line> lines, Width width);
+    void tune(Ladder ladder, Height height, Width width);
 }

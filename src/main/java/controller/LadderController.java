f6456245287663d8191314492078cf474d7ea5ba
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-07 16:06:18 +0900

    feat: 사다리 게임 결과 기능 추가

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -5,12 +5,16 @@ import generator.RandomGenerator;
 import ladder.Height;
 import ladder.LadderGame;
 import ladder.Width;
+import result.LadderResult;
 import strategy.LinkStrategy;
 import strategy.RandomLinkStrategy;
 import tuner.DefaultLadderTuner;
 import tuner.LadderTuner;
 import view.InputView;
 import view.OutputView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LadderController {
     public void run() {
@@ -24,5 +28,13 @@ public class LadderController {
         LadderGame game = new LadderGame(height, width, strategy, tuner);
 
         OutputView.printLadder(game.getLadder());
+
+        //사다리 게임 결과 출력
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        for (int i = 0; i < width.width(); i++) {
+            resultMap.put(i, game.play(i));
+        }
+        LadderResult result = new LadderResult(resultMap);
+        OutputView.printResult(result);
     }
 }
--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -13,6 +13,32 @@ public class LadderGame {
     public Ladder getLadder() {
         return ladder;
     }
+
+    public int play(int start) {
+        int position = start;
+        for (int row = 0; row < ladder.getLines().size(); row++) {
+            position = movePosition(position, ladder.getLines().get(row));
+        }
+        return position;
+    }
+
+    private int movePosition(int position, Line line) {
+        if (canMoveLeft(position, line)) {
+            return position - 1;
+        }
+        if (canMoveRight(position, line)) {
+            return position + 1;
+        }
+        return position;
+    }
+
+    private boolean canMoveLeft(int position, Line line) {
+        return position > 0 && line.isLinkedAt(position - 1);
+    }
+
+    private boolean canMoveRight(int position, Line line) {
+        return position < line.getLinks().size() && line.isLinkedAt(position);
+    }
 }
 
 

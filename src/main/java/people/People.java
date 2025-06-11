71528a2ac6b41ad7aa451d44a318e4799796de93
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 22:57:51 +0900

    feat: contains, indexOf 메소드 추가

--- src/main/java/people/People.java
+++ src/main/java/people/People.java
@@ -2,6 +2,7 @@ package people;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class People {
     private final List<Person> people;
@@ -11,6 +12,12 @@ public class People {
     }
 
     public static People from(List<String> names) {
+        // 중복 이름 체크
+        long uniqueCount = names.stream().distinct().count();
+        if (uniqueCount != names.size()) {
+            throw new IllegalArgumentException("중복된 이름은 허용되지 않습니다.");
+        }
+
         List<Person> people = names.stream()
                 .map(Person::from)
                 .toList();
@@ -21,7 +28,21 @@ public class People {
         return people.size();
     }
 
+    public boolean contains(String name) {
+        return people.stream()
+                .anyMatch(person -> person.name().equals(name));
+    }
+
+    public int indexOf(String name) {
+        return IntStream.range(0, people.size())
+                .filter(i -> people.get(i).isSameName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이름입니다."));
+    }
+
     public List<Person> values() {
         return Collections.unmodifiableList(people);
     }
+
 }
+

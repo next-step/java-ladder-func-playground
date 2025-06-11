19d509039d798e3c111a91d42cb385c0781e80b4
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 21:06:43 +0900

    feat: readName, readPrize 메소드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,15 +5,30 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int readLadderWidth(){
-        System.out.println("사다리의 넓이는 몇 개인가요?");
-        return parseToInt(scanner.nextLine());
+    public static String readName(){
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        return scanner.nextLine();
+    }
 
+//    public static int readLadderWidth(){
+//        System.out.println("사다리의 넓이는 몇 개인가요?");
+//        return parseToInt(scanner.nextLine());
+//
+//    }
+
+    public static String readPrize(){
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return scanner.nextLine();
     }
 
     public static int readLadderHeight(){
-        System.out.println("사다리의 높이는 몇 개인가요?");
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return parseToInt(scanner.nextLine());
+    }
+
+    public static String readResultName(){
+        System.out.println("결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
     }
 
     private static int parseToInt(String input) {

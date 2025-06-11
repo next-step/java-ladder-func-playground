ea5970f621e0565f4db7f8536c25d58dd261594d
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 18:30:44 +0900

    refactor: 예외 메시지에 .formatted() 적용

--- src/main/java/ladder/Column.java
+++ src/main/java/ladder/Column.java
@@ -5,7 +5,9 @@ public record Column(int value) {
 
     public Column {
         if (value < MIN_COLUMN) {
-            throw new IllegalArgumentException("사다리의 넓이는 " + MIN_COLUMN + " 이상이어야 합니다.");
+            throw new IllegalArgumentException(
+                    "사다리의 넓이는 %d 이상이어야 합니다.".formatted(MIN_COLUMN)
+            );
         }
     }
 
--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -4,8 +4,10 @@ public record Row(int value) {
     private static final int MIN_ROW = 1;
 
     public Row {
-        if (value < MIN_ROW  ) {
-            throw new IllegalArgumentException("사다리의 높이는 "+ MIN_ROW  + " 이상이어야 합니다.");
+        if (value < MIN_ROW) {
+            throw new IllegalArgumentException(
+                    "사다리의 높이는 %d 이상이어야 합니다.".formatted(MIN_ROW)
+            );
         }
     }
 

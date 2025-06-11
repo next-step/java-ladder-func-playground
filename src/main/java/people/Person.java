df4d997906f526a91350b3ec7602677b78f04b1a
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 22:54:44 +0900

    feat: isSameName 메소드 추가

--- src/main/java/people/Person.java
+++ src/main/java/people/Person.java
@@ -4,6 +4,10 @@ public record Person(String name) {
     private static final int MAX_NAME_LENGTH = 5;
 
     public Person {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어 있을 수 없습니다.");
+        }
+
         if (name.length() >  MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(
                     "이름의 길이는 %d 이하여야 합니다.".formatted(MAX_NAME_LENGTH)
@@ -13,5 +17,9 @@ public record Person(String name) {
 
     public static Person from(String name) {
         return new Person(name);
+    }
+
+    public boolean isSameName(String target) {
+        return this.name.equals(target);
     }
 }

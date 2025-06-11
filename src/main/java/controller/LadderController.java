360ac8f744d63f3c8bf6367ac722fa0ebdb47388
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 21:08:01 +0900

    feat: LadderController에 참여하는 사람 및 실행 결과 로직 구현

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -5,22 +5,37 @@ import generator.RandomGenerator;
 import ladder.Row;
 import ladder.LadderGame;
 import ladder.Column;
+import people.People;
 import result.LadderResult;
+import result.Prizes;
 import strategy.LinkStrategy;
 import strategy.RandomLinkStrategy;
 import tuner.DefaultLadderTuner;
 import tuner.LadderTuner;
+import view.InputParser;
 import view.InputView;
 import view.OutputView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LadderController {
     public void run() {
-        Column columns = Column.from(InputView.readLadderWidth());
+        //1. 참여할 사람 이름을 입력
+        List<String> names = InputParser.parseCommaSeparated(InputView.readName());
+        People people = People.from(names);
+
+        Column columns = Column.from(people.size());
+
+        //2. 실행 결과 입력
+        List<String> results = InputParser.parseCommaSeparated(InputView.readPrize());
+        Prizes prizes = Prizes.from(results);
+
+        //3. 최대 사다리 높이 입력
         Row rows = Row.from(InputView.readLadderHeight());
 
+        //4. 사다리 생성
         Generator generator = new RandomGenerator();
         LinkStrategy strategy = new RandomLinkStrategy(generator);
 

6b89b99e6c67ee9d635f2997f9819120d6ccf006
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-11 21:05:33 +0900

    feat: InputParser 구현

---
+++ src/main/java/view/InputParser.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    public static List<String> parseCommaSeparated(String input) {
+        return Arrays.stream(input.strip().split(","))
+                .map(String::strip)
+                .toList();
+    }
+}

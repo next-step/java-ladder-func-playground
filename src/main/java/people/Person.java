f8d8bd01671d8230285b4440d222bfdd7d55dad0
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-14 18:01:23 +0900

    feat: 특정 이름 금지 로직 추가

--- src/main/java/people/Person.java
+++ src/main/java/people/Person.java
@@ -2,6 +2,7 @@ package people;
 
 public record Person(String name) {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String INVALID_NAME = "all";
 
     public Person {
         if (name == null || name.isBlank()) {
@@ -12,6 +13,9 @@ public record Person(String name) {
             throw new IllegalArgumentException(
                     "이름의 길이는 %d 이하여야 합니다.".formatted(MAX_NAME_LENGTH)
             );
+        }
+        if (name.equalsIgnoreCase(INVALID_NAME)) {
+            throw new IllegalArgumentException("\"%s\"은 사용할 수 없는 이름입니다.".formatted(INVALID_NAME));
         }
     }
 

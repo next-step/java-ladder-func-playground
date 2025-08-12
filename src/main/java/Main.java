2c31d3243a649fc62aee47f1db6aac2cd86c9219
dldb-chamchi <dladkfl49@gmail.com>
2025-08-12 15:01:00 +0900

    feat: Main 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.LadderController;
+
+public class Main {
+    public static void main(String[] args) {
+        LadderController ladderGameController = new LadderController();
+        ladderGameController.start();
+    }
+}

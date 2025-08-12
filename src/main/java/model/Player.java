f1a49169c18cc93d15215989351297e2ef4cf586
dldb-chamchi <dladkfl49@gmail.com>
2025-08-12 20:47:59 +0900

    refactor: 매직넘버 상수화

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,10 +1,12 @@
 package model;
 
 public class Player {
+
     private final String name;
+    private static final int MAX_NAME_LENGTH = 5;
 
     public Player(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 최대 5글자까지 가능합니다.");
         }
         this.name = name;

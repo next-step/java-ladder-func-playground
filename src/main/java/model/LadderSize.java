ad7f0fe3ff249ffc1de722d5ac757a1b2a376ec0
이고은 <6jtgoeun@naver.com>
2025-10-29 12:14:39 +0900

    refector:LadderSize: 안쓰는 부분들 제거

--- src/main/java/model/LadderSize.java
+++ src/main/java/model/LadderSize.java
@@ -5,18 +5,11 @@ public class LadderSize {
     private final int width;
 
     public LadderSize(int width, int height) {
-        if (height <= 0) {
-            throw new IllegalArgumentException("높이는 1 이상이여야 합니다.");
-        }
         this.height = height;
         this.width = width;
     }
 
     public int height() {
         return height;
-    }
-
-    public int width() {
-        return width;
     }
 }

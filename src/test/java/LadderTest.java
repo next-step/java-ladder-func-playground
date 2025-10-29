86309598d7d3d7b062fdadfe36fedbd19a363b81
이고은 <6jtgoeun@naver.com>
2025-10-29 14:18:02 +0900

    병합 전 로컬 변경사항 저장

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("사다리 테스트")

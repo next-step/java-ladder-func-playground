21cd60e14cae191dcd750a6fbb8b05f16482844e
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-10-29 22:33:58 +0900

    chore: LinePoints를 만드는 제너레이터 생성

--- src/main/java/domain/LadderBuilder.java
+++
@@ -1,4 +0,0 @@
-package java.domain;
-
-public class LadderBuilder {
-}
---
+++ src/main/java/domain/LinePointsGenerator.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LinePointsGenerator {
+    private final Random random = new Random();
+
+    public Line generateLine(int colCount) {
+        List<Boolean> points = new ArrayList<>(colCount - 1);
+        for (int i = 0; i < colCount - 1; i++) {
+            if (i > 0 && points.get(i - 1)) points.add(false);
+            else points.add(random.nextBoolean());
+        }
+        return new Line(points);
+    }
+}
+

0876fa9befe39decd3e2334442f7bb33a97d084e
이고은 <6jtgoeun@naver.com>
2025-10-29 13:54:00 +0900

    refector:와일드카드 선언부 수정

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -10,7 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("사다리 테스트")
 public class LadderTest {

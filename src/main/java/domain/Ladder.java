2149b73ce9f63a6f8ba6f30c43e4f9a9d18c3e8a
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-10-29 22:33:33 +0900

    chore: Line,Ladder값객체 생성

---
+++ src/main/java/domain/Ladder.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,14 @@
+package domain;
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> points;
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
+}

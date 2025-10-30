4ae4ed7679f5cfd92edea47c18acbaae552bc7c9
nonactress <tjgsuwls1007@gmail.com>
2025-10-30 11:30:48 +0900

    feat : 플레이어 관리 클래스

---
+++ src/main/java/Model/Player.java
@@ -0,0 +1,41 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Player extends SplitMethod{
+    List<String> players;
+    List<Integer> destinatioinPos = new ArrayList<>();
+
+    public Player(String players){
+        this.players = split(players);
+    }
+
+    public List<String> getPlayers() {
+        return Collections.unmodifiableList(players);
+    }
+
+    public int getDestinatioinPos(String string) {
+        return destinatioinPos.get(players.indexOf(string));
+    }
+
+    public int getPlayersNumber()
+    {
+        return players.size();
+    }
+
+    public void playerOnDestination(Bridge bridge)
+    {
+        LadderDescentService descentService = new LadderDescentService(bridge);
+
+        for (String player : players) {
+            destinatioinPos.add(descentService.descent(players.indexOf(player)));
+        }
+    }
+
+
+
+}

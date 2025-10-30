712256274a2ae5a2ec3c641dd3d042ed8b931602
kimsky247-coder <kimsky247@gmail.com>
2025-10-30 16:51:18 +0900

    feat(Controller): 사다리 게임 컨트롤러 구현

---
+++ src/main/java/controller/LadderGameController.java
@@ -0,0 +1,104 @@
+package controller;
+
+import model.result.Prize;
+import model.result.Prizes;
+import model.participant.Player;
+import model.participant.Players;
+import model.ladder.Ladder;
+import model.ladder.LadderGame;
+import model.result.GameResult;
+import view.InputView;
+import view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class LadderGameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void startGame(){
+        try {
+            Players players = setupPlayers();
+            Prizes prizes = setupPrizes(players);
+            Ladder ladder = setupLadder(players.size());
+            LadderGame ladderGame = new LadderGame(ladder);
+
+            GameResult gameResult = calculateAllResults(ladderGame, players, prizes);
+
+            outputView.printLadder(ladder, players, prizes);
+            startResultQueryLoop(gameResult, players);
+
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            outputView.printError(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputView.printError("알 수 없는 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+
+    private Players setupPlayers(){
+        String namesLine = inputView.inputPeople();
+        List<String> playerNames = Arrays.asList(namesLine.split(","));
+
+        List<Player> playerList = new ArrayList<>();
+        for(String playerName : playerNames){
+            playerList.add(new Player(playerName.trim()));
+        }
+        return new Players(playerList);
+    }
+
+    private Prizes setupPrizes(Players players){
+        String prizeLine = inputView.inputResults();
+        List<String> prizesType = Arrays.asList(prizeLine.split(","));
+
+        List<Prize> prizeList = new ArrayList<>();
+        for(String prize : prizesType){
+            prizeList.add(new Prize(prize.trim()));
+        }
+        Prizes prizes = new Prizes(prizeList);
+        validateInput(players, prizes);
+        return prizes;
+    }
+    private void validateInput(Players players, Prizes prizes){
+        if(players.size() != prizes.size()){
+            throw new IllegalArgumentException("이름 수와 결과 수는 같아야 합니다.");
+        }
+    }
+
+    private Ladder setupLadder(int numberOfPeople){
+        while (true) {
+            try {
+                int height = inputView.inputLadderHeight();
+                return new Ladder(numberOfPeople, height);
+            } catch (NumberFormatException e) {
+                outputView.printError("숫자만 입력할 수 있습니다");
+            }
+        }
+    }
+
+    private GameResult calculateAllResults(LadderGame ladderGame, Players players, Prizes prizes) {
+        GameResult gameResult = new GameResult();
+        int numberOfPeople = players.size();
+
+        for (int startColumn = 0; startColumn < numberOfPeople; startColumn++) {
+            Player currentPlayer = players.getPlayerAt(startColumn);
+            int resultColumn = ladderGame.trace(startColumn);
+            Prize currentPrize = prizes.getPrizeAt(resultColumn);
+
+            gameResult.addResult(currentPlayer, currentPrize);
+        }
+        return gameResult;
+    }
+
+    private void startResultQueryLoop(GameResult result, Players players) {
+        String playerResult = inputView.inputPlayerName();
+        while (!Objects.equals(playerResult, "all")) {
+            outputView.printSingleResult(result, playerResult);
+            playerResult = inputView.inputPlayerName();
+        }
+        outputView.printAllResults(result, players);
+    }
+}

cb13095eb2ce67a5918b592d6583e9aa6f3e8f5b
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-10-30 14:49:12 +0900

    refactor: InputView 갱신

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class InputView {
     Scanner scanner = new Scanner(System.in);
 
-    public int getInputCol(){
+    public int getInputCol() {
         return scanner.nextInt();
     }
 
-    public int getInputRow(){
+    public int getInputRow() {
         return scanner.nextInt();
+    }
+
+    public String getInputPlayers() {
+        return scanner.nextLine();
+    }
+
+    public String getInputSelectedPlayer() {
+        return scanner.nextLine();
+    }
+
+    public String getInputPrizes() {
+        return scanner.nextLine();
     }
 }

e2f073b63be76acf871ae34ebda559517257df66
nonactress <tjgsuwls1007@gmail.com>
2025-10-30 11:31:07 +0900

    feat : 도착지에 있는 보상 클래스

---
+++ src/main/java/Model/Rewards.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Rewards extends SplitMethod{
+    List<String> rewards;
+
+    public Rewards(String rewards) {
+        this.rewards = split(rewards);
+    }
+
+    public String getRewardskey(int index)
+    {
+        return rewards.get(index);
+    }
+    public List<String> getRewards() {
+        return Collections.unmodifiableList(rewards);
+    }
+}

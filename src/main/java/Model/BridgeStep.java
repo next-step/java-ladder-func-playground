25114d98a3d474f39df10b8dd2f2198389fe305e
nonactress <tjgsuwls1007@gmail.com>
2025-10-30 11:30:15 +0900

    feat : BridgeStep

---
+++ src/main/java/Model/BridgeStep.java
@@ -0,0 +1,28 @@
+// BridgeStep.java
+package Model;
+
+import java.util.Random;
+
+
+
+public enum BridgeStep {
+    NONE(0),  // 다리 없음
+    EXIST(1); // 다리 있음
+
+    private final int value;
+
+    BridgeStep(int value) {
+        this.value = value;
+    }
+
+    public static BridgeStep createRandomStep(Random random) {
+        if (random.nextBoolean()) {
+            return EXIST;
+        }
+        return NONE;
+    }
+
+    public boolean isExist() {
+        return this == EXIST;
+    }
+}

1514d48ebb839aa96336e259cfc1f446d7a7d64e
taewoo <dnxorla03@naver.com>
2025-10-31 22:36:26 +0900

    Line에 일급 컬렉션(Point) 적용 및 Connect enum으로 의미 전달 명확화

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -5,43 +5,43 @@ import java.util.List;
 import java.util.Random;
 
 public class Line {
-    private final List<Boolean> points;
+    private final List<Point> points;
 
-    private Line(List<Boolean> points) {
+    private Line(List<Point> points) {
         this.points = List.copyOf(points);
     }
 
     public static Line create(int playerCount, Random random) {
-        List<Boolean> points = new ArrayList<>();
-        boolean prev = false;
+        List<Point> points = new ArrayList<>();
+        Connect prev = Connect.DISCONNECTED;
 
         for (int i = 0; i < playerCount - 1; i++) {
-            boolean next = random.nextBoolean();
+            Connect next = Connect.from(random.nextBoolean());
             next = checkPrev(prev, next);
-            points.add(next);
+            points.add(new Point(next));
             prev = next;
         }
         return new Line(points);
     }
 
-    private static boolean checkPrev(boolean prev, boolean next) {
-        if (prev) {
-            next = false;
+    private static Connect checkPrev(Connect prev, Connect next) {
+        if (prev.isConnected()) {
+            return Connect.DISCONNECTED;
         }
         return next;
     }
 
-    public boolean validateMoveRight(int index) {
-        if (index >= points.size()) return false;
-        return points.get(index);
+    public Connect validateMoveRight(int index) {
+        if (index >= points.size()) return Connect.DISCONNECTED;
+        return points.get(index).point();
     }
 
-    public boolean validateMoveLeft(int index) {
-        if (index == 0) return false;
-        return points.get(index - 1);
+    public Connect validateMoveLeft(int index) {
+        if (index == 0) return Connect.DISCONNECTED;
+        return points.get(index - 1).point();
     }
 
-    public List<Boolean> getPoints() {
+    public List<Point> getPoints() {
         return points;
     }
 }

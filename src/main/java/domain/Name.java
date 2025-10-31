54ddd4d10a92b1cd742a70d988d6792fc334f01f
taewoo <dnxorla03@naver.com>
2025-10-31 22:37:19 +0900

    입력값을 원시값으로 포장하는 Name 클래스 추가

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,35 +1,34 @@
 package domain;
 
-import java.util.Objects;
-
 public class Name {
     private final String value;
-    public Name(String value){
-        if (value == null || value.trim().isEmpty()){
+
+    public Name(String value) {
+        if (value == null || value.trim().isEmpty()) {
             throw new IllegalArgumentException("비어 있을 수 없습니다.");
         }
         this.value = value.trim();
     }
 
-    public String value(){
+    public String value() {
         return value;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return value;
     }
 
     @Override
-    public boolean equals(Object obj){
+    public boolean equals(Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Name)) return false;
-        Name name = (Name)obj;
+        Name name = (Name) obj;
         return value.equals(name.value);
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return value.hashCode();
     }
 }

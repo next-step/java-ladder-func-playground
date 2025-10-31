af22352470e4838705524da49daa37d4317f8e73
taewoo <dnxorla03@naver.com>
2025-10-31 22:34:08 +0900

    높이가 0인 경우에도 사다리 한 줄이 생성되도록 보정

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -7,9 +7,13 @@ import java.util.Random;
 public class Ladder {
     private final List<Line> lines;
 
-    public Ladder(int height, int playerCount, Random random) {
+    public Ladder(Height height, int playerCount, Random random) {
         List<Line> temp = new ArrayList<>();
-        for (int i = 0; i < height; i++) {
+
+        for (int i = 0; i < height.getHeight(); i++) {
+            temp.add(Line.create(playerCount, random));
+        }
+        if (height.getHeight() == 0) {
             temp.add(Line.create(playerCount, random));
         }
         this.lines = List.copyOf(temp);

ecac217fe7919c4d8ed19ec94d794659b604b762
taewoo <dnxorla03@naver.com>
2025-10-31 22:35:19 +0900

    원시값 포장 및 일급 컬렉션 적용으로 인한 LadderGame 로직 수정

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -10,13 +10,12 @@ public class LadderGame {
     private final Results results;
 
     public LadderGame(Ladder ladder, Players players, Results results) {
-        validatePlayerAndResultCount(players, results);
         this.ladder = ladder;
         this.players = players;
         this.results = results;
     }
 
-    private void validatePlayerAndResultCount(Players players, Results results) {
+    public static void validatePlayerAndResultCount(Players players, Results results) {
         if (players.size() != results.size()) {
             throw new IllegalArgumentException("참가자와 결과 수는 같아야 합니다.");
         }
@@ -30,25 +29,25 @@ public class LadderGame {
     }
 
     private int calculateNextPosition(Line line, int position) {
-        if ((line.validateMoveRight(position))) return 1;
-        if ((line.validateMoveLeft(position))) return -1;
+        if (line.validateMoveRight(position).isConnected()) return 1;
+        if (line.validateMoveLeft(position).isConnected()) return -1;
         return 0;
     }
 
     public String findResultByPlayer(String name) {
-        List<String> player = players.getPlayers().getValues();
-        List<String> result = results.getResults().getValues();
+        List<Name> player = players.getPlayers().getValues();
+        List<Name> result = results.getResults().getValues();
 
-        int index = player.indexOf(name);
+        int index = player.indexOf(new Name(name));
         index = move(index);
-        return result.get(index);
+        return result.get(index).value();
     }
 
     public Map<String, String> findAll() {
         Map<String, String> map = new HashMap<>();
-        List<String> player = players.getPlayers().getValues();
-        for (String name : player) {
-            map.put(name, findResultByPlayer(name));
+        List<Name> player = players.getPlayers().getValues();
+        for (Name name : player) {
+            map.put(name.value(), findResultByPlayer(name.value()));
         }
         return map;
     }

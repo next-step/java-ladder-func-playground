64252616600ab1510ada61763619ce47b68dd71a
taewoo <dnxorla03@naver.com>
2025-10-31 14:14:56 +0900

    Players 이름 중복 제거 로직 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,21 +1,40 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Players {
     private final Names players;
 
     public Players(String input) {
-        this.players = new Names(input);
+        this.players = removeDuplicate(input);
         validatePlayerNames();
     }
 
+    private Names removeDuplicate(String input) {
+        String[] tokens = input.split(",");
+        List<Name> newInput = new ArrayList<>();
+
+        for (String token : tokens) {
+            String trimmed = token.trim();
+            if (!trimmed.isEmpty()) {
+                Name name = new Name(trimmed);
+                if (!newInput.contains(name)) {
+                    newInput.add(name);
+                }
+            }
+        }
+        return new Names(newInput);
+    }
+
     private void validatePlayerNames() {
-        for (String name : players.getValues()) {
+        for (Name name : players.getValues()) {
             validateNameLength(name);
         }
     }
 
-    private void validateNameLength(String name) {
-        if (name.length() > 5) {
+    private void validateNameLength(Name name) {
+        if (name.value().length() > 5) {
             throw new IllegalArgumentException("이름은 5글자를 넘기면 안됩니다.");
         }
     }

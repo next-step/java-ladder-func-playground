7ed8f0ad4976d5fdfcf66eaf6426e533b69a38ae
nonactress <tjgsuwls1007@gmail.com>
2025-11-02 22:26:14 +0900

    refactor(outputView) : Bridge와 BridgeRow로 리팩토링

---
+++ src/main/java/Model/BridgeRow.java
@@ -0,0 +1,57 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Collections;
+
+public class BridgeRow {
+
+    private final List<BridgeStep> steps;
+
+    public BridgeRow(int width, Random random) {
+        this.steps = createOneRow(width, random);
+    }
+
+    private List<BridgeStep> createOneRow(int width, Random random) {
+        List<BridgeStep> row = generateRandomRow(width, random);
+        ensureNoSideBySideSteps(row, random);
+        return row;
+    }
+
+    private List<BridgeStep> generateRandomRow(int width, Random random) {
+        List<BridgeStep> row = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            row.add(BridgeStep.createRandomStep(random));
+        }
+        return row;
+    }
+
+    private void ensureNoSideBySideSteps(List<BridgeStep> row, Random random) {
+        for (int i = 0; i < row.size() - 1; i++) {
+            fixSideBySideStepIfNeeded(row, i, random);
+        }
+    }
+
+    private void fixSideBySideStepIfNeeded(List<BridgeStep> row, int index, Random random) {
+        if (areBothStepsExisting(row, index)) {
+            setOneToNoneRandomly(row, index, random);
+        }
+    }
+
+    private boolean areBothStepsExisting(List<BridgeStep> row, int index) {
+        return row.get(index).isExist() && row.get(index + 1).isExist();
+    }
+
+    private void setOneToNoneRandomly(List<BridgeStep> row, int index, Random random) {
+        if (random.nextBoolean()) {
+            row.set(index, BridgeStep.NONE);
+            return;
+        }
+        row.set(index + 1, BridgeStep.NONE);
+    }
+
+    public List<BridgeStep> getSteps() {
+        return Collections.unmodifiableList(this.steps);
+    }
+}

04b80c8752f7d27e8a709004c133b629fc1daef7
nonactress <tjgsuwls1007@gmail.com>
2025-11-02 22:31:42 +0900

    refactor(outputView) : Bridge와 BridgeRow로 리팩토링2

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,26 +1,22 @@
 package view;
 
-
-import Model.Bridge;
-import Model.BridgeStep;
-import Model.Player;
-import Model.Rewards;
+import Model.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class OutputView {
+
     public void printBridge(Bridge bridge) {
-        for (List<BridgeStep> row : bridge.getRows()) {
-            printBrigdeRow(row);
+        for (BridgeRow row : bridge.getRows()) {
+            printBridgeRow(row);
             System.out.print("|");
             System.out.println();
         }
-
     }
 
-    private void printBrigdeRow(List<BridgeStep> row) {
-        row.forEach(bridgeStep -> {
+    private void printBridgeRow(BridgeRow row) {
+        row.getSteps().forEach(bridgeStep -> {
             System.out.print("|");
             printBridgeStep(bridgeStep);
         });
@@ -51,7 +47,6 @@ public class OutputView {
                 });
     }
 
-
     public void printSpecificRewards(Rewards rewards, Player players, String specificNames) {
         Arrays.stream(specificNames.split(","))
                 .map(String::trim)
@@ -64,28 +59,6 @@ public class OutputView {
                     }
                 });
     }
-
-
-//    public boolean printRewards(Rewards rewards, Player players, String inputNames) {
-//        System.out.println("실행 결과");
-//        String trimmedInput = inputNames.trim();
-//        if (trimmedInput.equals("all")) {
-//            players.getPlayers().stream()
-//                    .forEach(player -> System.out.println(player + " : " + rewards.getRewardskey(players.getDestinatioinPos(player))));
-//            return true;
-//        }
-//
-//        Arrays.stream(trimmedInput.split(","))
-//                .map(String::trim)
-//                .forEach(trimmedName -> {
-//                    try {
-//                        System.out.println(trimmedName + " : " + rewards.getRewardskey(players.getDestinatioinPos(trimmedName)));
-//                    } catch (Exception e) {
-//                        throw new IllegalArgumentException("[ERROR] 존재하지 않는 플레이어 이름입니다: " + trimmedName);
-//                    }
-//                });
-//        return false;
-//    }
 
     public void askPlayers() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");

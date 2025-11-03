931719247c2ed593043e96bbae9159bf83412b2e
taewoo <dnxorla03@naver.com>
2025-11-03 17:53:47 +0900

    Connect enum에 이동 책임을 부여하여 도메인 응집도 개선

--- src/main/java/domain/Connect.java
+++ src/main/java/domain/Connect.java
@@ -1,20 +1,44 @@
 package domain;
 
 public enum Connect {
-    CONNECTED(true),
-    DISCONNECTED(false);
+    CONNECTED(true) {
+        @Override
+        public int moveRight(int index) {
+            return index + 1;
+        }
+
+        @Override
+        public int moveLeft(int index) {
+            return index - 1;
+        }
+    },
+    DISCONNECTED(false) {
+        @Override
+        public int moveRight(int index) {
+            return index;
+        }
+
+        @Override
+        public int moveLeft(int index) {
+            return index;
+        }
+    };
 
     private final boolean value;
 
-    Connect(boolean value){
+    Connect(boolean value) {
         this.value = value;
     }
 
-    public boolean isConnected(){
+    public abstract int moveRight(int index);
+
+    public abstract int moveLeft(int index);
+
+    public boolean isConnected() {
         return value;
     }
 
-    public static  Connect from(boolean value){
+    public static Connect from(boolean value) {
         return value ? CONNECTED : DISCONNECTED;
     }
 }

00e58d3226a77a1e693ac741b63b7b3868ad4c98
이고은 <6jtgoeun@naver.com>
2025-11-03 23:57:55 +0900

    1차수정 - 테스트코드실패-> 유효성 검증로직 추가

--- src/main/java/model/LadderSize.java
+++ src/main/java/model/LadderSize.java
@@ -5,8 +5,15 @@ public class LadderSize {
     private final int width;
 
     public LadderSize(int width, int height) {
+        validateHeight(height);
         this.height = height;
         this.width = width;
+    }
+
+    private void validateHeight(int height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException("사다리 높이는 1 이상이어야 합니다.");
+        }
     }
 
     public int height() {

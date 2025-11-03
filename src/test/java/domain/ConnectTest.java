4db9002091d727c31fa8e6b6fbf01d7fe604f38b
taewoo <dnxorla03@naver.com>
2025-11-03 18:02:40 +0900

    이동 로직 테스트 추가

--- src/test/java/domain/ConnectTest.java
+++ src/test/java/domain/ConnectTest.java
@@ -22,4 +22,42 @@ class ConnectTest {
         //then
         assertThat(value).isEqualTo(Connect.DISCONNECTED);
     }
+
+    @Test
+    void CONNECTED_왼쪽으로_이동한다() {
+        //give
+        int index = 3;
+
+        //when
+        int result = Connect.CONNECTED.moveLeft(index);
+
+        //then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void CONNECTED_오른쪽으로_이동한다() {
+        //give
+        int index = 3;
+
+        //when
+        int result = Connect.CONNECTED.moveRight(index);
+
+        //then
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void DISCONNECTED_제자리이다() {
+        //give
+        int index = 3;
+
+        //when
+        int result1 = Connect.DISCONNECTED.moveRight(index);
+        int result2 = Connect.DISCONNECTED.moveLeft(index);
+
+        //then
+        assertThat(result1).isEqualTo(3);
+        assertThat(result2).isEqualTo(3);
+    }
 }

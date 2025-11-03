339963b237a78a8f2429e8b2f18b3c955fd352c7
taewoo <dnxorla03@naver.com>
2025-11-03 17:55:19 +0900

    Height 객체 제거에 따른 Ladder 생성부 및 연관 로직 수정

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -7,13 +7,13 @@ import java.util.Random;
 public class Ladder {
     private final List<Line> lines;
 
-    public Ladder(Height height, int playerCount, Random random) {
+    public Ladder(int height, int playerCount, Random random) {
         List<Line> temp = new ArrayList<>();
 
-        for (int i = 0; i < height.getHeight(); i++) {
+        for (int i = 0; i < height; i++) {
             temp.add(Line.create(playerCount, random));
         }
-        if (height.getHeight() == 0) {
+        if (height == 0) {
             temp.add(Line.create(playerCount, random));
         }
         this.lines = List.copyOf(temp);

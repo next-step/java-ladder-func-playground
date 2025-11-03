e023d6de36fdc9c409bcf5141ebcd2b08bf2ee38
taewoo <dnxorla03@naver.com>
2025-11-03 18:05:52 +0900

    PlayerName 기반으로 PlayersTest 수정

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -2,48 +2,32 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayersTest {
+
     @Test
-    void 쉼표로_구분된_문자열_생성() {
-        //given
-        String input = "태우1,태우2";
+    void 플레이어_리스트를_생성할_수_있다() {
+        // given
+        List<PlayerName> list = List.of(new PlayerName("태우1"), new PlayerName("태우2"));
 
-        //when
-        Players players = new Players(input);
+        // when
+        Players players = new Players(list);
 
-        //then
+        // then
         assertThat(players.size()).isEqualTo(2);
-        assertThat(players.getPlayers().getValues())
-                .extracting(Name::value)
-                .containsExactly("태우1", "태우2");
     }
 
     @Test
-    void 이름_5글자_초과시_예외발생() {
-        //given
-        String input = "태우태우태우";
+    void 중복된_이름이_있으면_예외() {
+        // given
+        List<PlayerName> list = List.of(new PlayerName("태우"), new PlayerName("태우"));
 
-        //when & then
-        assertThatThrownBy(() -> new Players(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름은 5글자를 넘기면 안됩니다.");
-    }
-
-    @Test
-    void 중복된_이름은_저장하지_않는다() {
-        //given
-        String input = "태우,태우,태우1";
-
-        //when
-        Players players = new Players(input);
-
-        //then
-        assertThat(players.size()).isEqualTo(2);
-        assertThat(players.getPlayers().getValues())
-                .extracting(Name::value)
-                .containsExactly("태우", "태우1");
+        // when & then
+        assertThatThrownBy(() -> new Players(list))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

fa9f491b13d984cc12c61360f264119b771f23bc
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-11-03 23:31:47 +0900

    refactor: players 일급리스트 사용

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -25,15 +25,21 @@ public class Ladder {
     }
 
     private void validateCols(int cols) {
-        if (cols < 2) throw new IllegalArgumentException("사다리 너비(cols)는 2 이상이어야 합니다.");
+        if (cols < 2){
+            throw new IllegalArgumentException("사다리 너비(cols)는 2 이상이어야 합니다.");
+        }
     }
 
     private void validateRows(int rows) {
-        if (rows < 1) throw new IllegalArgumentException("사다리 높이(rows)는 1 이상이어야 합니다.");
+        if (rows < 1){
+            throw new IllegalArgumentException("사다리 높이(rows)는 1 이상이어야 합니다.");
+        }
     }
 
     private void validateLines(List<LinePoints> lines, int rows) {
-        if (lines.size() != rows) throw new IllegalArgumentException("라인 개수와 rows가 일치하지 않습니다.");
+        if (lines.size() != rows){
+            throw new IllegalArgumentException("라인 개수와 rows가 일치하지 않습니다.");
+        }
     }
 
 }

ff4df5e4b113920fc1e4a740fd22f058fe11e4d0
taewoo <dnxorla03@naver.com>
2025-11-03 18:04:50 +0900

    Line 이동 규칙을 moveOf 기준으로 재구성 및 테스트 케이스 보강

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,35 +19,89 @@ class LineTest {
 
         //then
         for (int i = 0; i < line.getPoints().size() - 1; i++) {
-            boolean prev = line.getPoints().get(i).point().isConnected();
-            boolean next = line.getPoints().get(i + 1).point().isConnected();
+            boolean prev = line.getPoints().get(i).isConnected();
+            boolean next = line.getPoints().get(i + 1).isConnected();
             assertThat(prev && next).isFalse();
         }
     }
 
     @Test
-    void 사다리가_맨끝인_경우_DISCONNECTED이다() {
+    void 라인_마지막_인덱스면_오른쪽이동불가() {
         //given
-        int index = 3;
-        Line line = Line.create(4, new Random(1));
+        int index = 2;
+        Line line = Line.of(List.of(
+                Connect.CONNECTED,
+                Connect.DISCONNECTED
+        ));
 
         //when
-        Connect result = line.validateMoveRight(index);
+        int result = line.moveOf(index);
 
         //then
-        assertThat(result).isEqualTo(Connect.DISCONNECTED);
+        assertThat(result).isEqualTo(index);
     }
 
     @Test
-    void 사다리가_처음인_경우_DISCONNECTED이다() {
+    void 라인_처음_인덱스면_왼쪽이동불가() {
         //given
+        Line line = Line.of(List.of(
+                Connect.DISCONNECTED,
+                Connect.CONNECTED
+        ));
         int index = 0;
-        Line line = Line.create(4, new Random(1));
 
         //when
-        Connect result = line.validateMoveLeft(index);
+        int result = line.moveOf(index);
 
         //then
-        assertThat(result).isEqualTo(Connect.DISCONNECTED);
+        assertThat(result).isEqualTo(index);
+    }
+
+    @Test
+    void 오른쪽은_DISCONNECTED_왼쪽은_CONNECTED면_왼쪽_으로_이동한다() {
+        // given
+        Line line = Line.of(List.of(
+                Connect.DISCONNECTED,
+                Connect.CONNECTED
+        ));
+        int index = 2;
+
+        // when
+        int result = line.moveOf(index);
+
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void 오른쪽은_CONNECTED_왼쪽은_DISCONNECTED면_오른쪽_으로_이동한다() {
+        // given
+        Line line = Line.of(List.of(
+                Connect.DISCONNECTED,
+                Connect.CONNECTED
+        ));
+        int index = 1;
+
+        // when
+        int result = line.moveOf(index);
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void 양쪽모두_DISCONNECTED면_제자리() {
+        //give
+        Line line = Line.of(List.of(
+                Connect.DISCONNECTED,
+                Connect.DISCONNECTED
+        ));
+        int index = 1;
+
+        //when
+        int result = line.moveOf(index);
+
+        //then
+        assertThat(result).isEqualTo(index);
     }
 }

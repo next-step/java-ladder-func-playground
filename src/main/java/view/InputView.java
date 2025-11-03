be7153f8c505975f91edadafe08b5b9cd7752e79
이고은 <6jtgoeun@naver.com>
2025-11-03 23:53:54 +0900

    1차수정 - 입력 시 띄어쓰기 무시하기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,13 +10,13 @@ public class InputView {
     public List<String> inputParticipants() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String input = scanner.nextLine();
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(",\\s*"));
     }
 
     public List<String> inputResults() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         String input = scanner.nextLine();
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(",\\s*"));
     }
 
     public int heightSize() {
@@ -29,6 +29,6 @@ public class InputView {
     public String inputQueryName() {
         System.out.println();
         System.out.println("결과를 보고 싶은 사람은?");
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
 }

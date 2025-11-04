9d9be5b122b3e664003dca3f82709f7a8cd642c6
kimsky247-coder <kimsky247@gmail.com>
2025-11-04 22:46:37 +0900

    style: 코드 포맷팅 수정 (개행 및 들여쓰기)

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -91,7 +91,6 @@ public class OutputView {
         }
     }
 
-
     public void printError(String errorMessage) {
         printer.printError(errorMessage);
     }
--- src/test/java/ladder/generator/FakeConnectionGenerator.java
+++ src/test/java/ladder/generator/FakeConnectionGenerator.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
 public class FakeConnectionGenerator implements ConnectionGenerator {
 
     private final Queue<Boolean> predefinedValues;

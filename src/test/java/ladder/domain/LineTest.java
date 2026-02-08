d101e0030f95f9883631bf47dedc076c9de28095
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-08 22:57:32 +0900

    test: move에 대한 테스트코드 작성 - Point를 3개로 지정하고 전략을 true로 하여 기둥이 true, false, true로 생성되게 함 - 각 기둥에서 이동하여 도착한 인덱스를 검증

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -48,4 +48,23 @@ public class LineTest {
         assertThat(line.getPoints()).hasSize(size);
     }
 
+    @DisplayName("다리 유무에 따라 인덱스가 좌, 우로 이동하거나 그대로 유지된다.")
+    @Test
+    void move() {
+        Line line = Line.from(3, () -> true);
+
+        assertAll(
+            // 0번 기둥: 0번 포인트가 T이므로 오른쪽(1)으로 이동
+            () -> assertThat(line.move(0)).isEqualTo(1),
+
+            // 1번 기둥: 0번 포인트가 T이므로 왼쪽(0)으로 이동
+            () -> assertThat(line.move(1)).isEqualTo(0),
+
+            // 2번 기둥: 2번 포인트가 T이므로 오른쪽(3)으로 이동 (1번 포인트는 F)
+            () -> assertThat(line.move(2)).isEqualTo(3),
+
+            // 3번 기둥: 2번 포인트가 T이므로 왼쪽(2)으로 이동
+            () -> assertThat(line.move(3)).isEqualTo(2)
+        );
+    }
 }

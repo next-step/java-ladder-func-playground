33f1f4f49375efc3ac16babd22382c12ef2e2dcc
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-09 01:00:17 +0900

    feat: 사다리 게임 참가자 및 결과 입력 처리 로직 추가

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,9 +1,13 @@
 package ladder;
 
+import java.util.Map;
 import java.util.Random;
+import ladder.controller.LadderGameController;
 import ladder.domain.Ladder;
+import ladder.domain.LadderGameResult;
 import ladder.domain.LadderHeight;
-import ladder.domain.LadderWidth;
+import ladder.domain.LadderResults;
+import ladder.domain.Participants;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
@@ -12,12 +16,20 @@ public class Application {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        LadderWidth width = InputView.inputWidth();
+        Participants participants = InputView.inputNames();
+        LadderResults results = InputView.inputLadderResults(participants.size());
+
         LadderHeight height = InputView.inputHeight();
 
-        Ladder ladder = Ladder.of(width, height, RANDOM::nextBoolean);
+        Ladder ladder = Ladder.of(participants, height, RANDOM::nextBoolean);
 
-        OutputView.printLadder(ladder);
+        Map<Integer, Integer> path = ladder.generateResults();
+
+        LadderGameResult gameResult = LadderGameResult.of(participants, results, path);
+
+        OutputView.printLadder(participants, ladder, results);
+        LadderGameController.runInquiry(gameResult);
+
     }
 
 }

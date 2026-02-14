0827136cc280a025049bbbd6c3ab4baf31542ac9
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-14 19:02:43 +0900

    refactor: Point 클래스에 대해 Enum으로 변경 - 언어 차원에서 싱글톤 보장 - 객체보다는 상태임을 명시

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -1,24 +1,18 @@
 package ladder.domain;
 
-public class Point {
-
-    private static final Point BRIDGE = new Point(true);
-    private static final Point EMPTY = new Point(false);
+public enum Point {
+    BRIDGE(true),
+    EMPTY(false);
 
     private final boolean hasBridge;
 
-    private Point(boolean hasBridge) {
+    Point(boolean hasBridge) {
         this.hasBridge = hasBridge;
     }
 
     public static Point from(boolean hasBridge) {
-        if (hasBridge) {
-            return BRIDGE;
-        }
-        return EMPTY;
+        return hasBridge ? BRIDGE : EMPTY;
     }
 
-    public boolean hasBridge() {
-        return hasBridge;
-    }
+    public boolean hasBridge() { return hasBridge; }
 }

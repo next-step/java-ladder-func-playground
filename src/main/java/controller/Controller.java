1e8e4190882d854cf321db0202689a8cb7965a38
chaehyunL <chaehyun1012@gmail.com>
2026-04-22 23:18:54 +0900

    feat:4단계 구현 부분 추가 생성

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,19 +2,38 @@ package controller;
 
 import model.Ladder;
 import model.LadderResult;
+import util.StringUtil;
 import view.InputView;
 import view.OutputView;
 
-public class Controller {
-    private final InputView inputView=new InputView();
-    private final OutputView outputView=new OutputView();
-    public void run(){
-        int width=inputView.getWidth();
-        int height=inputView.getHeight();
-        Ladder ladder=new Ladder(height,width);
-        outputView.printLadder(ladder);
+import java.util.List;
 
-        LadderResult ladderResult=new LadderResult(width,ladder);
-        outputView.printLadderResult(ladderResult);
+public class Controller {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        String names = inputView.readNames();
+        String targets = inputView.readTargets();
+
+        List<String> nameList = StringUtil.splitByComma(names);
+        List<String> targetList = StringUtil.splitByComma(targets);
+
+        int width = nameList.size() - 1;
+        int height = inputView.readHeight();
+
+
+        Ladder ladder = new Ladder(height, width);
+        outputView.printLadder(ladder, nameList, targetList);
+
+        LadderResult ladderResult = new LadderResult(width, ladder, nameList, targetList);
+
+        String getTarget1 = inputView.readTargetResult();
+
+        outputView.printTarget(getTarget1, ladderResult, nameList);
+
+        String getTarget2 = inputView.readTargetResult();
+
+        outputView.printTarget(getTarget2, ladderResult, nameList);
     }
 }

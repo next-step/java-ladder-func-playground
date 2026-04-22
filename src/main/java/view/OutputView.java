83325edb63b79c1d6d91afa5ab9c86bc370360df
chaehyunL <chaehyun1012@gmail.com>
2026-04-22 23:05:03 +0900

    feat: target 출력을 2회 반복하는 것을 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,35 +2,58 @@ package view;
 
 import model.Ladder;
 import model.LadderResult;
-
 import model.Line;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.BlockingDeque;
+
 
 public class OutputView {
 
-    public void printLadder(Ladder ladder){
-        System.out.println("실행결과");
-        for(Line line: ladder.getLadder()){
+    public void printLadder(Ladder ladder, List<String> nameList, List<String> targetList) {
+        System.out.println("\n사다리결과");
+        printNames(nameList);
+        System.out.println(" ");
+        for (Line line : ladder.getLadder()) {
             printLine(line);
         }
+        printTargets(targetList);
     }
 
-    public void printLine(Line line){
+    public void printNames(List<String> nameList) {
+        for (String name : nameList) {
+            System.out.printf("%-6s", name);
+        }
+        System.out.println();
+    }
+
+    public void printTargets(List<String> targetList) {
+        for (String target : targetList) {
+            System.out.printf("%-6s", target);
+        }
+        System.out.println();
+    }
+
+    public void printLine(Line line) {
         System.out.print("ㅣ");
-        for(int i=0;i< line.getWidth();i++){
+        for (int i = 0; i < line.getWidth(); i++) {
             System.out.print(line.getLine().get(i) ? "-----ㅣ" : "     ㅣ");
         }
         System.out.println(" ");
     }
 
-    public void printLadderResult(LadderResult ladderResult){
-        for(Map.Entry<Integer,Integer> entry:ladderResult.getLadderResult().entrySet()){
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+    public void printTarget(String input, LadderResult ladderResult, List<String> names) {
+        if (input.equals("all")) {
+            printTotalResult(ladderResult, names);
+        } else {
+            System.out.println("\n실행 결과");
+            System.out.println(ladderResult.getTargetLadderResult(input.trim()));
         }
     }
 
+    public void printTotalResult(LadderResult ladderResult, List<String> names) {
+        System.out.println("\n실행 결과");
+        for (String name : names) {
+            System.out.println(name + " : " + ladderResult.getTargetLadderResult(name));
+        }
+    }
 }

7a6cee60d3230f002177eb58684dc57c721f1b02
chaehyunL <chaehyun1012@gmail.com>
2026-04-22 19:51:28 +0900

    feat: LineGenerator.java 생성

---
+++ src/main/java/util/LineGenerator.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LineGenerator {
+    private final static Random random=new Random();
+    public static List<Boolean> makeLine(int width) {
+        List<Boolean> points = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            boolean value;
+            if (i > 0 && points.get(i - 1)) {
+                value = false;
+            } else {
+                value = random.nextBoolean();
+            }
+            points.add(value);
+        }
+        return points;
+    }
+}

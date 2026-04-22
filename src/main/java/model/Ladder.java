5cf8598356d93dadfdbb26fae58d4fe0b322dc29
chaehyunL <chaehyun1012@gmail.com>
2026-04-22 19:50:37 +0900

    feat: Ladder.java 생성

---
+++ src/main/java/model/Ladder.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> ladder = new ArrayList<>();
+    int width;
+    int height;
+    public Ladder(int height, int width) {
+        this.width=width;
+        this.height=height;
+        makeLadder();
+    }
+
+    private void makeLadder() {
+        for (int i = 0; i < height; i++) {
+            Line line = new Line(width);
+            ladder.add(line);
+        }
+    }
+
+    public List<Line> getLadder() {
+        return ladder;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+}
+

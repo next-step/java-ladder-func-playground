0e36cf894f27bf6259ec5a0c173bd80e8b2ddef5
dahyn <k0d5h16@gmail.com>
2026-04-23 17:03:12 +0900

    feat: Player 이름 검증 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,12 +2,15 @@ package domain;
 
 public record Player(String name, int position) {
     public Player {
-        validate(name);
+        validateName(name);
     }
 
-    private void validate(String name) {
+    private void validateName(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("플레이어 이름이 입력되지 않았습니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("플레이어 이름은 5글자 이하입니다.");
         }
     }
 }

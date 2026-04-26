8677bd5aaed4ac2a3c417082a63f5f8fecdc2d6d
chaehyunL <chaehyun1012@gmail.com>
2026-04-26 16:07:24 +0900

    feat:형식 검증 로직 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,23 +1,43 @@
 package view;
 
+import validator.FormatValidator;
+
 import java.util.Scanner;
 
 public class InputView {
     Scanner scanner = new Scanner(System.in);
 
     public String readNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return scanner.nextLine();
+        while (true) {
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            String input = scanner.nextLine();
+            if (FormatValidator.validateCommaSeparatedFormat(input)) {
+                return input;
+            }
+            System.out.println("[ERROR] 올바른 형식이 아닙니다.");
+        }
     }
 
     public String readTargets() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        return scanner.nextLine();
+        while (true) {
+            String input = scanner.nextLine();
+            if (FormatValidator.validateCommaSeparatedFormat(input)) {
+                return input;
+            }
+            System.out.println("[ERROR] 올바른 형식이 아닙니다.");
+        }
     }
 
     public String readHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return scanner.nextLine();
+        while(true){
+            System.out.println("최대 사다리 높이는 몇 개인가요?");
+            String input=scanner.nextLine();
+            if(FormatValidator.validateIntegerFormat(input)){
+                return input;
+            }
+            System.out.println("[ERROR] 숫자값을 입력해주세요.");
+        }
     }
 
     public String readTargetResult() {

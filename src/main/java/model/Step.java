150a479266ee96e9264cedda22e356546cd214a8
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-04-28 01:21:58 +0900

    refactor : Step을 record로 수정

--- src/main/java/model/Step.java
+++ src/main/java/model/Step.java
@@ -1,15 +1,6 @@
 package model;
 
-public class Step {
-    private final boolean isBlank;
-
-    public Step(boolean isBlank){
-        this.isBlank = isBlank;
-    }
-
-    public boolean isBlank() {
-        return isBlank;
-    }
+public record Step(boolean isBlank) {
 
     @Override
     public String toString() {

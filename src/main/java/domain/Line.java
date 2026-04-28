5f81de0b56b6ba81b355708fe29073900f42997d
mgim9316-a11y <mgim9316@gmail.com>
2026-04-28 12:57:38 +0900

    feat: 사다리의 규칙을 관리하는 기능 추가

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -11,26 +11,65 @@ public class Line {
         this.points = points;
     }
 
-    public static Line generate(LadderWidth width, BooleanGenerator generator) {
+    public static Line generateFirst(LadderWidth width, BooleanGenerator generator) {
         List<Boolean> points = new ArrayList<>();
         boolean previous = false;
         for (int i = 0; i < width.getIntervalCount(); i++) {
-            previous = addPointAndReturnPrevious(points, previous, generator);
+            previous = addPoint(points, previous, false, generator);
         }
         return new Line(points);
     }
 
-    private static boolean addPointAndReturnPrevious(List<Boolean> points, boolean previous, BooleanGenerator generator) {
-        boolean nextPoint = generateNext(previous, generator);
+    public static Line generateNext(LadderWidth width, BooleanGenerator generator, Line previousLine) {
+        List<Boolean> points = new ArrayList<>();
+        boolean previous = false;
+        for (int i = 0; i < width.getIntervalCount(); i++) {
+            boolean above = previousLine.isConnectedAt(i);
+            previous = addPoint(points, previous, above, generator);
+        }
+        return new Line(points);
+    }
+
+    private static boolean addPoint(List<Boolean> points, boolean previous, boolean above, BooleanGenerator gen) {
+        boolean nextPoint = determineNext(previous, above, gen);
         points.add(nextPoint);
         return nextPoint;
     }
 
-    private static boolean generateNext(boolean previous, BooleanGenerator generator) {
-        if (previous) {
+    private static boolean determineNext(boolean previous, boolean above, BooleanGenerator gen) {
+        if (previous || above) {
             return false;
         }
-        return generator.generate();
+        return gen.generate();
+    }
+
+    public boolean isConnectedAt(int index) {
+        return points.get(index);
+    }
+
+    public Position move(Position position) {
+        int currentIndex = position.getValue();
+        if (canMoveLeft(currentIndex)) {
+            return position.moveLeft();
+        }
+        if (canMoveRight(currentIndex)) {
+            return position.moveRight();
+        }
+        return position;
+    }
+
+    private boolean canMoveLeft(int currentIndex) {
+        if (currentIndex <= 0) {
+            return false;
+        }
+        return points.get(currentIndex - 1);
+    }
+
+    private boolean canMoveRight(int currentIndex) {
+        if (currentIndex >= points.size()) {
+            return false;
+        }
+        return points.get(currentIndex);
     }
 
     public List<Boolean> getPoints() {

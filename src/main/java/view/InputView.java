ea9a9a0e899c8c08a816c62638692df0d2acbc32
mgim9316-a11y <mgim9316@gmail.com>
2026-04-28 13:11:59 +0900

    feat: 쉼표(,)를 기준으로 문자열을 분리하여 사람 이름과 결과를 입력받는 메서드 추가. 결과를 확인할 사람의 이름을 입력받는  메서드 추가.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,19 +1,51 @@
 package view;
 
+import domain.PlayerName;
+import domain.Players;
+import domain.Reward;
+import domain.Rewards;
+
 import java.util.Scanner;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
-    private static final String REQUEST_WIDTH_MESSAGE = "사다리의 넓이는 몇 개인가요?";
-    private static final String REQUEST_HEIGHT_MESSAGE = "사다리의 높이는 몇 개인가요?";
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public int readWidth() {
-        System.out.println(REQUEST_WIDTH_MESSAGE);
-        return Integer.parseInt(SCANNER.nextLine());
+    public Players readPlayers() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        return new Players(parseNames(SCANNER.nextLine()));
+    }
+
+    private List<PlayerName> parseNames(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(PlayerName::new)
+                .collect(Collectors.toList());
+    }
+
+    public Rewards readRewards() {
+        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return new Rewards(parseRewards(SCANNER.nextLine()));
+    }
+
+    private List<Reward> parseRewards(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Reward::new)
+                .collect(Collectors.toList());
     }
 
     public int readHeight() {
-        System.out.println(REQUEST_HEIGHT_MESSAGE);
+        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public String readTargetPerson() {
+        System.out.println("\n결과를 보고 싶은 사람은?");
+        return SCANNER.nextLine().trim();
     }
 }

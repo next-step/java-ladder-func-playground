821e9d22d3d9cafc134c62d7dafedbfa3ae737fe
mgim9316-a11y <mgim9316@gmail.com>
2026-04-28 13:10:19 +0900

    feat: 1. 참가자 이름과 실행 결과 입력 로직 추가. 2. 사다리 생성 시 폭(Width)을 입력받은 사람의 수(players.size())로 동적 할당하도록 변경. 3. 특정 사람의 이름 혹은 "all" 입력에 따라 결과를 반복 출력하는 로직 추가

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -5,24 +5,52 @@ import view.InputView;
 import view.OutputView;
 
 public class LadderController {
-    private final OutputView outputView;
     private final InputView inputView;
+    private final OutputView outputView;
 
     public LadderController(InputView inputView, OutputView outputView) {
-        this.outputView = outputView;
         this.inputView = inputView;
-
+        this.outputView = outputView;
     }
 
     public void run() {
-        LadderWidth width = new LadderWidth(inputView.readWidth());
+        Players players = inputView.readPlayers();
+        Rewards rewards = inputRewards(players);
         LadderHeight height = new LadderHeight(inputView.readHeight());
-        BooleanGenerator generator = new RandomBooleanGenerator();
 
-        Ladder ladder = Ladder.generate(width, height, generator);
-        outputView.printLadder(ladder);
+        Ladder ladder = Ladder.generate(new LadderWidth(players.size()), height, new RandomBooleanGenerator());
+        outputView.printLadderBoard(players, ladder, rewards);
 
-        LadderResult result = ladder.play(width);
-        outputView.printLadderResult(result);
+        GameResult gameResult = createGameResult(players, rewards, ladder);
+        printTargetResults(gameResult);
+    }
+
+    private Rewards inputRewards(Players players) {
+        Rewards rewards = inputView.readRewards();
+        players.validateMatch(rewards);
+        return rewards;
+    }
+
+    private GameResult createGameResult(Players players, Rewards rewards, Ladder ladder) {
+        LadderResult ladderResult = ladder.play(new LadderWidth(players.size()));
+        return GameResult.of(players, rewards, ladderResult);
+    }
+
+    private void printTargetResults(GameResult gameResult) {
+        while (true) {
+            String target = inputView.readTargetPerson();
+            if (processTarget(target, gameResult)) {
+                break;
+            }
+        }
+    }
+
+    private boolean processTarget(String target, GameResult gameResult) {
+        if ("all".equals(target)) {
+            outputView.printAllResults(gameResult);
+            return true;
+        }
+        outputView.printSingleResult(gameResult.findByName(target));
+        return false;
     }
 }

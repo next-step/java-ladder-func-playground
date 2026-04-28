035a320680b3e63264f0ebc405e2aa69f36e5b6a
mgim9316-a11y <mgim9316@gmail.com>
2026-04-28 13:14:49 +0900

    feat: 시작 위치를 전달받아 매핑된 도착 위치를 반환하는메서드 추가.

--- src/main/java/domain/LadderResult.java
+++ src/main/java/domain/LadderResult.java
@@ -3,11 +3,16 @@ package domain;
 import java.util.Collections;
 import java.util.Map;
 
+
 public class LadderResult {
     private final Map<Position, Position> results;
 
     public LadderResult(Map<Position, Position> results) {
         this.results = results;
+    }
+
+    public Position getEndPosition(Position start) {
+        return results.get(start);
     }
 
     public Map<Position, Position> getResults() {

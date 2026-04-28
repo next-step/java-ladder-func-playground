6803d8425d9c2ce6ba3542967e3e40c2cb454a68
htdufhc-bob <htdufhc@gmail.com>
2026-04-28 10:25:18 +0900

    feat: ErrorMessage enum 추가

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,27 @@
+package constant;
+
+public enum ErrorMessage {
+    INVALID_INTEGER("정수를 입력하세요."),
+    NOT_POSITIVE_NUMBER("0보다 큰 값을 입력하세요."),
+    EMPTY_OR_BLANK_NAME("이름은 비어 있거나 공백일 수 없습니다."),
+    NOT_FOUND_NAME("존재하지 않는 이름입니다."),
+    PLAYER_NOT_FOUND("해당 참가자가 없습니다."),
+    INVALID_NAME_LENGTH("이름은 최대 %d글자까지 가능합니다."),
+    INVALID_LADDER_HEIGHT("사다리 높이는 %d 이상이어야 합니다."),
+    INVALID_LADDER_WIDTH("사다리 너비는 %d 이상이어야 합니다."),
+    MISMATCH_PLAYER_AND_PRIZE("인원 수와 상품 수는 같아야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getIntFormattedMessage(int num) {
+        return String.format(message, num);
+    }
+}

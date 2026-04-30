2e22d2c29669242673004ba10c7ffeeda8c21810
chaehyunL <chaehyun1012@gmail.com>
2026-04-30 11:40:37 +0900

    refector:for문 stream 변경

--- src/main/java/validator/ContentValidator.java
+++ src/main/java/validator/ContentValidator.java
@@ -6,12 +6,8 @@ public class ContentValidator {
     private  static final int MAX_NAME_LENGTH=5;
 
     public static boolean validateNameList(List<String>names){
-        for (String name : names) {
-            if (name.length()>MAX_NAME_LENGTH) {
-                return false;
-            }
-        }
-        return true;
+        return names.stream()
+                .allMatch(name->name.length()<=MAX_NAME_LENGTH);
     }
 
     public static boolean validateGetTarget(String input,List<String> nameList){

93d4c0c578bf2bc7c96db55e8efe9e7291bf1a3b
htdufhc-bob <htdufhc@gmail.com>
2026-04-30 11:08:01 +0900

    feat: 이름 중복 검증 로직 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,6 +1,10 @@
 package domain;
 
+import constant.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Players {
     private final List<Player> players;
@@ -28,5 +32,13 @@ public class Players {
     public boolean containsPlayer(Player targetPlayer) {
         return players.stream()
                 .anyMatch(player -> player.equals(targetPlayer));
+    }
+
+    private void validateNameDuplicate(List<String> players) {
+        Set<String> uniquePlayers = new HashSet<>(players);
+
+        if (uniquePlayers.size() != players.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_PLAYER_NAME.getMessage());
+        }
     }
 }

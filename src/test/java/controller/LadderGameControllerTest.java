05c6961dd334cec78b6e8e5e20e416291d9fa454
htdufhc-bob <htdufhc@gmail.com>
2026-04-30 11:25:48 +0900

    test: main 코드 변경 내용 적용

--- src/test/java/controller/LadderGameControllerTest.java
+++ src/test/java/controller/LadderGameControllerTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.strategy.BooleanGenerator;
+import dto.LadderResultResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,7 +12,7 @@ import view.OutputView;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -63,6 +64,6 @@ public class LadderGameControllerTest {
 
         // then
         verify(outputView).printTargetResult("꽝");
-        verify(outputView).printTotalTargetResult(anyMap());
+        verify(outputView).printTotalTargetResult(any(LadderResultResponse.class));
     }
 }
--- src/test/java/domain/LadderLowTest.java
+++ src/test/java/domain/LadderLowTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class LineTest {
+public class LadderLowTest {
     private final static int MIN_WIDTH = 2;
 
     @Test
     void 사다리의_너비가_2미만_이면_예외가_발생한다() {
-        assertThatThrownBy(() -> Line.create(1, () -> false))
+        assertThatThrownBy(() -> new LadderLow(1, () -> false))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_LADDER_WIDTH.getIntFormattedMessage(MIN_WIDTH));
     }
@@ -19,32 +19,32 @@ public class LineTest {
     @Test
     void 오른쪽에_가로줄이_있으면_인덱스가_1_증가한다() {
         // given-when
-        Line line = Line.create(2, () -> true);
+        LadderLow line = new LadderLow(2, () -> true);
         // then
-        assertThat(line.move(0)).isEqualTo(1);
+        assertThat(line.calculateNextPosition(0)).isEqualTo(1);
     }
 
     @Test
     void 왼쪽에_가로줄이_있으면_인덱스가_1_감소한다() {
         // given-when
-        Line line = Line.create(2, () -> true);
+        LadderLow line = new LadderLow(2, () -> true);
         // then
-        assertThat(line.move(1)).isEqualTo(0);
+        assertThat(line.calculateNextPosition(1)).isEqualTo(0);
     }
 
     @Test
     void 가로줄이_없으면_제자리에_머문다() {
         // given-when
-        Line line = Line.create(2, () -> false);
+        LadderLow line = new LadderLow(2, () -> false);
         // then
-        assertThat(line.move(0)).isEqualTo(0);
+        assertThat(line.calculateNextPosition(0)).isEqualTo(0);
     }
 
     @Test
     void 가로줄은_연속해서_생성되지_않는다() {
         // given-when
-        Line line = Line.create(4, () -> true);
+        LadderLow line = new LadderLow(4, () -> true);
         // then
-        assertThat(line.getPoints()).containsExactly(true, false, true);
+        assertThat(line.getConnections()).containsExactly(true, false, true);
     }
 }

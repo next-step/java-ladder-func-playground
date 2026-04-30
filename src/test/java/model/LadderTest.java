3d21dfc82d7f61fed14e68f251d66beb4852896e
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-05-01 01:47:07 +0900

    test: calculateSingleResultAsDtoCallsEachLineMoveAlongTheRowInOrder 구현 방식 수정

--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -36,42 +36,16 @@ class LadderTest {
     @DisplayName("calculateSingleResultAsDto는 각 Line의 moveAlongTheRow를 직전 결과를 인자로 순서대로 호출한다")
     void calculateSingleResultAsDtoCallsEachLineMoveAlongTheRowInOrder() {
         //given
-        List<Step> steps = List.of(new Step(false), new Step(true), new Step(false));
-        MoveTrackingLine firstLine = new MoveTrackingLine(steps, 2);
-        MoveTrackingLine secondLine = new MoveTrackingLine(steps, 3);
-        MoveTrackingLine thirdLine = new MoveTrackingLine(steps, 1);
+        List<Step> stepsFirst = List.of(new Step(false),new Step(true), new Step(true));
+        List<Step> stepsSecond = List.of(new Step(true), new Step(false), new Step(true));
+        List<Step> stepsThird = List.of(new Step(true), new Step(true), new Step(false));
+        Line firstLine = new Line(stepsFirst);
+        Line secondLine = new Line(stepsSecond);
+        Line thirdLine = new Line(stepsThird);
         Ladder ladder = new Ladder(List.of(firstLine, secondLine, thirdLine));
         //when
         LadderResultDto result = ladder.calculateSingleResultAsDto(0);
         //then
-        Assertions.assertEquals(1, firstLine.callCount);
-        Assertions.assertEquals(0, firstLine.receivedIndex);
-
-        Assertions.assertEquals(1, secondLine.callCount);
-        Assertions.assertEquals(2, secondLine.receivedIndex);
-
-        Assertions.assertEquals(1, thirdLine.callCount);
-        Assertions.assertEquals(3, thirdLine.receivedIndex);
-
-        Assertions.assertEquals(0, result.startIndex());
-        Assertions.assertEquals(1, result.endIndex());
-    }
-
-    private static class MoveTrackingLine extends Line {
-        private final Integer stubbedReturn;
-        int callCount = 0;
-        Integer receivedIndex;
-
-        MoveTrackingLine(List<Step> steps, Integer stubbedReturn) {
-            super(steps);
-            this.stubbedReturn = stubbedReturn;
-        }
-
-        @Override
-        public Integer moveAlongTheRow(Integer index) {
-            this.callCount++;
-            this.receivedIndex = index;
-            return stubbedReturn;
-        }
+        Assertions.assertEquals(3, result.endIndex());
     }
 }

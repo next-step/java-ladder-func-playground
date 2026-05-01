cec2e4f1d4969a13b6cad7533a17678a3228c6e3
mgim9316-a11y <mgim9316@gmail.com>
2026-05-01 12:23:18 +0900

    feat: 해시코드, equals 추가

--- src/main/java/domain/PlayerName.java
+++ src/main/java/domain/PlayerName.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class PlayerName {
     private static final int MAX_LENGTH = 5;
     private final String value;
@@ -13,5 +15,18 @@ public class PlayerName {
 
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayerName that = (PlayerName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }

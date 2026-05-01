dab500b073a28b1ec4d75f6604989a77a851c0f9
htdufhc-bob <htdufhc@gmail.com>
2026-05-01 15:50:17 +0900

    refactor: 정적 팩토리 메서드 적용

--- src/main/java/domain/LadderResult.java
+++ src/main/java/domain/LadderResult.java
@@ -10,8 +10,19 @@ import java.util.Map;
 public class LadderResult {
     private final Map<Player, String> results;
 
-    public LadderResult(Players players, Prizes prizes, List<Integer> endIndex) {
-        this.results = calculateResult(players, prizes.getPrizeNames(), endIndex);
+    private LadderResult(Map<Player, String> results) {
+        this.results = results;
+    }
+
+    public static LadderResult of(Players players, Prizes prizes, List<Integer> endIndex) {
+        Map<Player, String> results = new LinkedHashMap<>();
+        List<Player> playerList = players.getPlayers();
+
+        for (int i = 0; i < playerList.size(); i++) {
+            results.put(playerList.get(i), prizes.getPrizeNames().get(endIndex.get(i)));
+        }
+
+        return new LadderResult(results);
     }
 
     public Map<Player, String> getAllResults() {
@@ -26,16 +37,5 @@ public class LadderResult {
                 .map(Map.Entry::getValue)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.PLAYER_NOT_FOUND.getMessage()));
-    }
-
-    private Map<Player, String> calculateResult(Players players, List<String> prizesName, List<Integer> endIndex) {
-        Map<Player, String> results = new LinkedHashMap<>();
-        List<Player> playerList = players.getPlayers();
-
-        for (int i = 0; i < playerList.size(); i++) {
-            results.put(playerList.get(i), prizesName.get(endIndex.get(i)));
-        }
-
-        return results;
     }
 }

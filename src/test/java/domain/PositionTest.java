c7eb9f2f1a1f470448dbb99d4175517a1e8ee03a
mgim9316-a11y <mgim9316@gmail.com>
2026-05-01 13:02:36 +0900

    refactor: 테스트 기능 수정

--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -1,28 +1,19 @@
 package domain;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 class PositionTest {
 
     @Test
-    @DisplayName("왼쪽으로 이동한다.")
-    void moveLeft() {
+    void 왼쪽으로_이동하면_값이_1_감소한다() {
         Position position = new Position(1);
         assertThat(position.moveLeft()).isEqualTo(new Position(0));
     }
 
     @Test
-    @DisplayName("오른쪽으로 이동한다.")
-    void moveRight() {
+    void 오른쪽으로_이동하면_값이_1_증가한다() {
         Position position = new Position(1);
         assertThat(position.moveRight()).isEqualTo(new Position(2));
-    }
-
-    @Test
-    @DisplayName("동일한 값을 가지면 동등한 객체로 취급한다.")
-    void equals() {
-        assertThat(new Position(1)).isEqualTo(new Position(1));
     }
 }

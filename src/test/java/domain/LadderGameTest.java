d655b5c1965d930584fd0c7bccbb098ff7cc64e7
dahyn <k0d5h16@gmail.com>
2026-05-02 14:21:53 +0900

    test: 상수 네이밍 수정

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LadderGameTest {
     private static final int LADDER_WIDTH = 4;
     private static final int LADDER_HEIGHT = 3;
-    private static final List<Boolean> TFT_ROW = List.of(
+    private static final List<Boolean> TRUE_FALSE_TRUE_ROWS = List.of(
             true, false, true,
             true, false, true,
             true, false, true
@@ -19,7 +19,7 @@ public class LadderGameTest {
 
     @Test
     void 사다리_실행_결과의_위치에_있는_상품을_플레이어에게_매칭한다() {
-        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TFT_ROW));
+        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TRUE_FALSE_TRUE_ROWS));
         Players players = Players.from(List.of("pobi", "crong", "loopy", "eddy"));
         List<String> prize = List.of("crocodile", "bear", "fox", "beaver");
         LadderGame ladderGame = new LadderGame(ladder, players);
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -13,7 +13,7 @@ public class LadderTest {
     private static final int LADDER_HEIGHT = 3;
     private static final int START_POSITION = 0;
     private static final int EXPECTED_END_POSITION = 1;
-    private static final List<Boolean> TFT_ROW = List.of(
+    private static final List<Boolean> TRUE_FALSE_TRUE_ROWS = List.of(
             true, false, true,
             true, false, true,
             true, false, true
@@ -21,21 +21,21 @@ public class LadderTest {
 
     @Test
     void 사다리의_높이는_1이상이다() {
-        assertThatThrownBy(() -> Ladder.of(LADDER_WIDTH, 0, new TestConnectionGenerator(TFT_ROW)))
+        assertThatThrownBy(() -> Ladder.of(LADDER_WIDTH, 0, new TestConnectionGenerator(TRUE_FALSE_TRUE_ROWS)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리의 높이는 1이상이어야합니다.");
     }
 
     @Test
     void 입력한_높이만큼_사다리_행이_생성된다() {
-        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TFT_ROW));
+        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TRUE_FALSE_TRUE_ROWS));
 
         assertThat(ladder.toBooleanLists()).hasSize(LADDER_HEIGHT);
     }
 
     @Test
     void 사다리는_여러_Row를_위에서_아래로_순서대로_적용한다() {
-        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TFT_ROW));
+        Ladder ladder = Ladder.of(LADDER_WIDTH, LADDER_HEIGHT, new TestConnectionGenerator(TRUE_FALSE_TRUE_ROWS));
         int endPosition = ladder.rideLadder(START_POSITION);
         assertThat(endPosition).isEqualTo(EXPECTED_END_POSITION);
     }

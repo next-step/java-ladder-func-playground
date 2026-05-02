17877058cadaadb1b85521ab9e42487514da83b5
dahyn <k0d5h16@gmail.com>
2026-05-02 13:04:35 +0900

    test: Players 생성 검증 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -4,9 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class PlayersTest {
+
+    @Test
+    void 플레이어_이름_목록으로_Players를_생성한다() {
+        Players players = Players.from(List.of("pobi", "crong"));
+
+        assertThat(players.getPlayerNames()).isEqualTo(List.of("pobi", "crong"));
+    }
 
     @Test
     void 플레이어의_이름은_중복될_수_없다() {

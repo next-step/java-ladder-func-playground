70a2dc6410d9936c3f6f8b4773ed71c1e757deeb
chaehyunL <chaehyun1012@gmail.com>
2026-05-03 19:41:28 +0900

    refector: scanner final로 변수 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import validator.FormatValidator;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public String readNames() {
         while (true) {
